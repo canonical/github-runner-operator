c0ee0ff5e8b12a4aaed475bd23c5976d086d7db1
Chris Sanders <sanders.chris@gmail.com>
2021-04-07 10:34:29 -0500

    Charmcraft init

---
+++ src/charm.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# Copyright 2021 Chris
+# See LICENSE file for licensing details.
+
+"""Charm the service.
+
+Refer to the following post for a quick-start guide that will help you
+develop a new k8s charm using the Operator Framework:
+
+    https://discourse.charmhub.io/t/4208
+"""
+
+import logging
+
+from ops.charm import CharmBase
+from ops.main import main
+from ops.framework import StoredState
+
+logger = logging.getLogger(__name__)
+
+
+class GithubRunnerOperatorCharm(CharmBase):
+    """Charm the service."""
+
+    _stored = StoredState()
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
+        self._stored.set_default(things=[])
+
+    def _on_config_changed(self, _):
+        # Note: you need to uncomment the example in the config.yaml file for this to work (ensure
+        # to not just leave the example, but adapt to your configuration needs)
+        current = self.config["thing"]
+        if current not in self._stored.things:
+            logger.debug("found a new thing: %r", current)
+            self._stored.things.append(current)
+
+    def _on_fortune_action(self, event):
+        # Note: you need to uncomment the example in the actions.yaml file for this to work (ensure
+        # to not just leave the example, but adapt to your needs for actions commands)
+        fail = event.params["fail"]
+        if fail:
+            event.fail(fail)
+        else:
+            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+
+
+if __name__ == "__main__":
+    main(GithubRunnerOperatorCharm)
---
+++ tests/test_charm.py
@@ -0,0 +1,35 @@
+# Copyright 2021 Chris
+# See LICENSE file for licensing details.
+
+import unittest
+from unittest.mock import Mock
+
+from ops.testing import Harness
+from charm import GithubRunnerOperatorCharm
+
+
+class TestCharm(unittest.TestCase):
+    def test_config_changed(self):
+        harness = Harness(GithubRunnerOperatorCharm)
+        self.addCleanup(harness.cleanup)
+        harness.begin()
+        self.assertEqual(list(harness.charm._stored.things), [])
+        harness.update_config({"thing": "foo"})
+        self.assertEqual(list(harness.charm._stored.things), ["foo"])
+
+    def test_action(self):
+        harness = Harness(GithubRunnerOperatorCharm)
+        harness.begin()
+        # the harness doesn't (yet!) help much with actions themselves
+        action_event = Mock(params={"fail": ""})
+        harness.charm._on_fortune_action(action_event)
+
+        self.assertTrue(action_event.set_results.called)
+
+    def test_action_fail(self):
+        harness = Harness(GithubRunnerOperatorCharm)
+        harness.begin()
+        action_event = Mock(params={"fail": "fail this"})
+        harness.charm._on_fortune_action(action_event)
+
+        self.assertEqual(action_event.fail.call_args, [("fail this",)])
