94f145b6f6bc7e9c0f7a64d47595fdd923339c02
Cory Johns <johnsca@gmail.com>
2021-09-17 11:20:23 -0400

    Clean up some patching

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -5,27 +5,12 @@ import pytest
 from runner import RunnerManager
 
 
-def _rm_factory_wrapper(tmp_path, rm_factory, *args, **kwargs):
-    runner_manager = rm_factory(*args, **kwargs)
-    runner_manager.runner_bin_path = tmp_path / "runner.tgz"
-    runner_manager.env_file = tmp_path / "env"
-    return runner_manager
-
-
-@pytest.fixture
-def runner_manager(tmp_path):
-    return _rm_factory_wrapper(
-        tmp_path, RunnerManager, "org", "token", "app", "container"
-    )
-
-
 @pytest.fixture(autouse=True)
 def mocks(monkeypatch, tmp_path):
     monkeypatch.setattr("runner.GhApi", mock.MagicMock())
     monkeypatch.setattr("runner.pylxd", mock.MagicMock())
     monkeypatch.setattr("runner.requests", mock.MagicMock())
-    monkeypatch.setattr("runner.RunnerManager._check_output", mock.MagicMock())
     monkeypatch.setattr("runner.time", mock.MagicMock())
-    monkeypatch.setattr(
-        "charm.RunnerManager", partial(_rm_factory_wrapper, tmp_path, RunnerManager)
-    )
+    monkeypatch.setattr("runner.RunnerManager._check_output", mock.MagicMock())
+    monkeypatch.setattr("runner.RunnerManager.runner_bin_path", tmp_path / "runner.tgz")
+    monkeypatch.setattr("runner.RunnerManager.env_file", tmp_path / "env")
