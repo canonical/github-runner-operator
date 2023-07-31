e589d33c4461cb35dde8bae543dd2704a9fa128a
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2023-07-31 16:26:00 +0800

    Add checking of resource profile content

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -6,6 +6,7 @@
 import json
 from typing import Any
 
+import yaml
 from juju.unit import Unit
 
 from runner import Runner
@@ -35,16 +36,27 @@ async def check_resource_lxd_profile(unit: Unit, configs: dict[str, Any]):
         AssertionError: Unable to find a LXD profile with matching resource
             config.
     """
-    resource_profile_name = Runner._get_resource_profile_name(
-        configs["vm-cpu"]["value"], configs["vm-memory"]["value"], configs["vm-disk"]["value"]
-    )
+    cpu = configs["vm-cpu"]["value"]
+    mem = configs["vm-memory"]["value"]
+    disk = configs["vm-disk"]["value"]
+    resource_profile_name = Runner._get_resource_profile_name(cpu, mem, disk)
 
+    # Verify the profile exists.
     action = await unit.run("lxc profile list --format json")
     await action.wait()
     assert action.results["return-code"] == 0
     profiles = json.loads(action.results["stdout"])
     profile_names = [profile["name"] for profile in profiles]
     assert resource_profile_name in profile_names
+
+    # Verify the profile contains the correct resource settings.
+    action = await unit.run(f"lxc profile show {resource_profile_name}")
+    await action.wait()
+    assert action.results["return-code"] == 0
+    profile_content = yaml.safe_load(action.results["stdout"])
+    assert f"{cpu}" == profile_content["config"]["limits.cpu"]
+    assert mem == profile_content["config"]["limits.memory"]
+    assert disk == profile_content["devices"]["root"]["size"]
 
 
 @retry(tries=30, delay=30)
