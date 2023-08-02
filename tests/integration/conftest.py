8372f00feeed7938bdf85b74e1bf9c4aef499de0
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2023-08-02 09:48:15 +0800

    Add integration test for charm configuration (#92)
    * Add resource config integration tests
    
    * Split integration tests into smaller tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -15,10 +15,13 @@ from juju.application import Application
 from juju.model import Model
 from pytest_operator.plugin import OpsTest
 
+from tests.integration.helpers import assesrt_num_of_runners
+from tests.status_name import ACTIVE_STATUS_NAME
+
 
 @pytest.fixture(scope="module")
 def metadata() -> dict[str, Any]:
-    """Metadata information of the charm"""
+    """Metadata information of the charm."""
     metadata = Path("./metadata.yaml")
     data = yaml.safe_load(metadata.read_text())
     return data
@@ -26,12 +29,24 @@ def metadata() -> dict[str, Any]:
 
 @pytest.fixture(scope="module")
 def app_name() -> str:
+    """Randomized application name."""
     # Randomized app name to avoid collision when connecting to GitHub.
     return f"integration-{secrets.token_hex(4)}"
 
 
+@pytest_asyncio.fixture(scope="module")
+async def charm_path(ops_test: OpsTest, lxd_profile: Path) -> AsyncIterator[Path]:
+    """Path to the built charm.
+
+    Including lxd_profile fixture as an argument, will make lxd_profile.yaml
+    file present under the charm directory during the building of the charm.
+    """
+    yield await ops_test.build_charm(".")
+
+
 @pytest.fixture(scope="module")
 def path(pytestconfig: pytest.Config) -> str:
+    """Configured path setting."""
     path = pytestconfig.getoption("--path")
     assert path, "Please specify the --path command line option"
     return path
@@ -39,6 +54,7 @@ def path(pytestconfig: pytest.Config) -> str:
 
 @pytest.fixture(scope="module")
 def token(pytestconfig: pytest.Config) -> str:
+    """Configured token setting."""
     token = pytestconfig.getoption("--token")
     assert token, "Please specify the --token command line option"
     return token
@@ -46,6 +62,7 @@ def token(pytestconfig: pytest.Config) -> str:
 
 @pytest.fixture(scope="module")
 def token_alt(pytestconfig: pytest.Config, token: str) -> str:
+    """Configured token_alt setting."""
     token_alt = pytestconfig.getoption("--token-alt")
     assert token_alt, "Please specify the --token-alt command line option"
     assert token_alt != token, "Please specify a different token for --token-alt"
@@ -54,30 +71,39 @@ def token_alt(pytestconfig: pytest.Config, token: str) -> str:
 
 @pytest.fixture(scope="module")
 def http_proxy(pytestconfig: pytest.Config) -> str:
+    """Configured http_proxy setting."""
     http_proxy = pytestconfig.getoption("--http-proxy")
     return "" if http_proxy is None else http_proxy
 
 
 @pytest.fixture(scope="module")
 def https_proxy(pytestconfig: pytest.Config) -> str:
+    """Configured https_proxy setting."""
     https_proxy = pytestconfig.getoption("--https-proxy")
     return "" if https_proxy is None else https_proxy
 
 
 @pytest.fixture(scope="module")
 def no_proxy(pytestconfig: pytest.Config) -> str:
+    """Configured no_proxy setting."""
     no_proxy = pytestconfig.getoption("--no-proxy")
     return "" if no_proxy is None else no_proxy
 
 
 @pytest.fixture(scope="module")
 def model(ops_test: OpsTest) -> Model:
+    """Juju model used in the test."""
     assert ops_test.model is not None
     return ops_test.model
 
 
 @pytest_asyncio.fixture(scope="module")
 async def lxd_profile() -> AsyncIterator[Path]:
+    """File containing LXD profile for test mode.
+
+    The file needs to be in the charm directory while building a test version
+    of the charm.
+    """
     lxd_profile_path = Path("lxd-profile.yaml")
 
     lxd_profile_path.write_text(
@@ -104,18 +130,21 @@ devices:
 
 @pytest_asyncio.fixture(scope="module")
 async def app_no_token(
-    ops_test: OpsTest,
     model: Model,
+    charm_path: Path,
     app_name: str,
     path: str,
     http_proxy: str,
     https_proxy: str,
     no_proxy: str,
-    lxd_profile: Path,
 ) -> AsyncIterator[Application]:
-    charm = await ops_test.build_charm(".")
+    """Application with no token.
 
+    Test should ensure it returns with the application having no token and no
+    runner.
+    """
     subprocess.run(["sudo", "modprobe", "br_netfilter"])
+
     await model.set_config(
         {
             "juju-http-proxy": http_proxy,
@@ -124,13 +153,15 @@ async def app_no_token(
             "logging-config": "<root>=INFO;unit=DEBUG",
         }
     )
+
     application = await model.deploy(
-        charm,
+        charm_path,
         application_name=app_name,
         series="jammy",
         config={
             "path": path,
             "virtual-machines": 0,
+            "denylist": "10.10.0.0/16",
             "test-mode": "insecure",
         },
     )
@@ -140,11 +171,55 @@ async def app_no_token(
 
 
 @pytest_asyncio.fixture(scope="module")
-async def app(model: Model, app_no_token: Application, token: str) -> AsyncIterator[Application]:
+async def app_no_runner(
+    model: Model, app_no_token: Application, token: str
+) -> AsyncIterator[Application]:
+    """Application with no runner.
+
+    The application might not have runner binary downloaded.
+
+    Test should ensure it returns with the application in a good state and has
+    no runner.
+    """
+    unit = app_no_token.units[0]
+
     await app_no_token.set_config({"token": token})
     await model.wait_for_idle()
+    assert app_no_token.status == ACTIVE_STATUS_NAME
+
+    # Wait until there is no runner.
+    await assesrt_num_of_runners(unit, 0)
 
     yield app_no_token
 
     await app_no_token.set_config({"token": ""})
+    await model.wait_for_idle()
+
+
+@pytest_asyncio.fixture(scope="module")
+async def app(model: Model, app_no_runner: Application) -> AsyncIterator[Application]:
+    """Application with a single runner.
+
+    Test should ensure it returns with the application in a good state and has
+    one runner.
+    """
+    unit = app_no_runner.units[0]
+
+    action = await unit.run_action("update-runner-bin")
+    await action.wait()
+
+    await app_no_runner.set_config({"virtual-machines": "1"})
+    action = await unit.run_action("reconcile-runners")
+    await action.wait()
+    await model.wait_for_idle()
+    assert app_no_runner.status == ACTIVE_STATUS_NAME
+
+    # Wait until there is one runner.
+    await assesrt_num_of_runners(unit, 1)
+
+    yield app_no_runner
+
+    await app_no_runner.set_config({"virtual-machines": "0"})
+    action = await unit.run_action("reconcile-runners")
+    await action.wait()
     await model.wait_for_idle()
---
+++ tests/integration/helpers.py
@@ -0,0 +1,88 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Utilities for integration test."""
+
+import json
+from typing import Any
+
+import yaml
+from juju.unit import Unit
+
+from runner import Runner
+from runner_manager import RunnerManager
+from utilities import retry
+
+
+async def remove_runner_bin(unit: Unit):
+    """Remove runner binary."""
+    action = await unit.run(f"rm {RunnerManager.runner_bin_path}")
+    await action.wait()
+
+    # No file should exists under with the filename.
+    action = await unit.run(f"test -f {RunnerManager.runner_bin_path}")
+    await action.wait()
+    assert action.results["return-code"] != 0
+
+
+async def assert_resource_lxd_profile(unit: Unit, configs: dict[str, Any]):
+    """Check for LXD profile of the matching resource config.
+
+    Args:
+        unit: Unit instance to check for the LXD profile.
+        configs: Configs of the application.
+
+    Raises:
+        AssertionError: Unable to find a LXD profile with matching resource
+            config.
+    """
+    cpu = configs["vm-cpu"]["value"]
+    mem = configs["vm-memory"]["value"]
+    disk = configs["vm-disk"]["value"]
+    resource_profile_name = Runner._get_resource_profile_name(cpu, mem, disk)
+
+    # Verify the profile exists.
+    action = await unit.run("lxc profile list --format json")
+    await action.wait()
+    assert action.results["return-code"] == 0
+    profiles = json.loads(action.results["stdout"])
+    profile_names = [profile["name"] for profile in profiles]
+    assert resource_profile_name in profile_names
+
+    # Verify the profile contains the correct resource settings.
+    action = await unit.run(f"lxc profile show {resource_profile_name}")
+    await action.wait()
+    assert action.results["return-code"] == 0
+    profile_content = yaml.safe_load(action.results["stdout"])
+    assert f"{cpu}" == profile_content["config"]["limits.cpu"]
+    assert mem == profile_content["config"]["limits.memory"]
+    assert disk == profile_content["devices"]["root"]["size"]
+
+
+@retry(tries=30, delay=30)
+async def assesrt_num_of_runners(unit: Unit, num: int) -> None:
+    """Check if runner instances are ready.
+
+    Args:
+      app: Application instance to check the runners.
+      num: Number of runner instances to check for.
+
+    Raises:
+        AssertionError: Correct number of runners is not found within timeout
+            limit.
+    """
+    action = await unit.run("lxc list --format json")
+    await action.wait()
+    assert action.status == "completed"
+
+    assert action.results["return-code"] == 0
+
+    lxc_instance = json.loads(action.results["stdout"])
+    assert len(lxc_instance) == num
+
+    for instance in lxc_instance:
+        action = await unit.run(f"lxc exec {instance['name']} -- ps aux")
+        await action.wait()
+        assert action.status == "completed"
+
+        assert f"/bin/bash {Runner.runner_script}" in action.results["stdout"]
