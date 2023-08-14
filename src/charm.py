11593b158d38092253e37307f719d1af56373954
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2023-08-14 11:30:20 +0800

    Update the status during checking of update

--- src/charm.py
+++ src/charm.py
@@ -410,10 +410,13 @@ class GithubRunnerCharm(CharmBase):
         Returns:
             Whether the runner binary or the services was updated.
         """
+        self.unit.status = MaintenanceStatus("Checking for updates")
+
         runner_manager = self._get_runner_manager()
         if not runner_manager:
             return False
 
+        self.unit.status = MaintenanceStatus("Checking for service updates")
         service_updated = self._install_repo_policy_compliance()
 
         # Check if the runner binary file exists.
@@ -421,7 +424,7 @@ class GithubRunnerCharm(CharmBase):
             self._stored.runner_bin_url = None
 
         try:
-            self.unit.status = MaintenanceStatus("Checking for runner updates")
+            self.unit.status = MaintenanceStatus("Checking for runner binary updates")
             runner_info = runner_manager.get_latest_runner_bin_url()
         except urllib.error.URLError as err:
             logger.exception("Failed to check for runner updates")
