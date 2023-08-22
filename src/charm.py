a5ea52581a49184b0bd70116019fe48523e7c371
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2023-08-22 10:58:39 +0800

    Reformat

--- src/charm.py
+++ src/charm.py
@@ -320,7 +320,9 @@ class GithubRunnerCharm(CharmBase):
     def _upgrade_kernel(self) -> None:
         """Upgrade the Linux kernel."""
         # TMP: Use proposed kernel version.
-        execute_command(["/usr/bin/apt-add-repository", "ppa:canonical-kernel-team/proposed", "-y"])
+        execute_command(
+            ["/usr/bin/apt-add-repository", "-y", "ppa:canonical-kernel-team/proposed"]
+        )
 
         execute_command(["/usr/bin/apt-get", "update"])
         execute_command(["/usr/bin/apt-get", "install", "-qy", "linux-generic"])
