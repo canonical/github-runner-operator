e27d433a1977bbda508c664049a9340319f3bfde
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2023-08-28 13:21:37 +0800

    Test e2e test microk8s fix

--- src/runner.py
+++ src/runner.py
@@ -507,7 +507,13 @@ class Runner:
         self.instance.execute(["/usr/bin/chown", "ubuntu:ubuntu", str(self.env_file)])
 
         # Ensure the apparmor service is running.
-        self.instance.execute(["/usr/sbin/service", "snapd.apparmor", "start"])
+        self.instance.execute(["/usr/bin/systemctl", "start", "apparmor"])
+        self.instance.execute(
+            ["/usr/sbin/apparmor_parser", "-r", "/etc/apparmor.d/*snap-confine*"]
+        )
+        self.instance.execute(
+            ["/usr/sbin/apparmor_parser", "-r", "/var/lib/snapd/apparmor/profiles/snap-confine*"]
+        )
 
         if self.config.proxies:
             # Creating directory and putting the file are idempotent, and can be retried.
