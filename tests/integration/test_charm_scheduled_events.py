a216c671e2a4859dfbc87057dd4d89f8bf8eaa6b
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2023-09-05 16:41:57 +0800

    Add integration test for scheduled event

--- tests/integration/test_charm_scheduled_events.py
+++ tests/integration/test_charm_scheduled_events.py
@@ -14,7 +14,12 @@ from juju.application import Application
 from juju.model import Model
 
 from runner_manager import RunnerManager
-from tests.integration.helpers import check_runner_binary_exists, run_in_unit
+from tests.integration.helpers import (
+    check_runner_binary_exists,
+    get_runner_names,
+    run_in_unit,
+    wait_till_num_of_runners,
+)
 from tests.status_name import ACTIVE_STATUS_NAME
 
 
@@ -42,3 +47,31 @@ async def test_update_interval(model: Model, app_scheduled_events: Application) 
     await sleep(3 * 60)
     await model.wait_for_idle(status=ACTIVE_STATUS_NAME)
     assert await check_runner_binary_exists(unit)
+
+
+@pytest.mark.asyncio
+@pytest.mark.abort_on_fail
+async def test_reconcile_interval(model: Model, app_scheduled_events: Application) -> None:
+    """
+    arrange: An working application with one runner.
+    act:
+        1. Crash the one runner
+        2. Wait for 3 minutes, and then wait for ActiveStatus.
+    assert:
+        1. No runner exists.
+        2. One runner exists. The runner name should not be the same as the starting one.
+    """
+    unit = app_scheduled_events.units[0]
+
+    runner_names = await get_runner_names(unit)
+    assert len(runner_names) == 1
+    runner_name = runner_names[0]
+    await run_in_unit(unit, f"lxc stop --force {runner_name}")
+    await wait_till_num_of_runners(unit, 0)
+
+    await sleep(3 * 60)
+    await model.wait_for_idle(status=ACTIVE_STATUS_NAME)
+
+    runner_names = await get_runner_names(unit)
+    assert len(runner_names) == 1
+    assert runner_name != runner_names[0]
