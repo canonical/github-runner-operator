ed7c52c339a529961a23c9ba7fb218a97051b138
Christopher Bartz <christopher.bartz@canonical.com>
2023-11-14 10:04:09 +0100

    Quote variable

--- scripts/pre-integration-test.sh
+++ scripts/pre-integration-test.sh
@@ -11,7 +11,7 @@ loop_device=$(sudo losetup -f)
 
 # Install squid proxy for proxy test
 gateway_ip="$(ip route show 0.0.0.0/0 | grep -oP 'via \K\S+')"
-default_ip="$(ip route get $gateway_ip | grep -oP 'src \K\S+')"
+default_ip="$(ip route get "$gateway_ip" | grep -oP 'src \K\S+')"
 sudo apt install squid
 sudo sed -i 's/http_access deny/# http_access deny/g' /etc/squid/squid.conf
 echo "http_access allow all" | sudo tee -a /etc/squid/squid.conf
