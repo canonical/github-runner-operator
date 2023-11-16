4476080d2a8d6c15f7e07b308028af4595165c6e
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2023-11-16 16:08:43 +0800

    Add proxy configuration

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 /snap/bin/lxc launch ubuntu-daily:jammy runner --vm
 while ! /snap/bin/lxc exec runner -- /usr/bin/who
 do
@@ -35,6 +37,27 @@ done
 /snap/bin/lxc exec runner -- /usr/sbin/usermod -aG microk8s ubuntu
 /snap/bin/lxc exec runner -- /usr/sbin/usermod -aG docker ubuntu
 /snap/bin/lxc exec runner -- /usr/sbin/iptables -I DOCKER-USER -j ACCEPT
+
+# Set up aproxy for downloading
+/snap/bin/lxc exec runner -- /usr/bin/snap install aproxy --edge
+/snap/bin/lxc exec runner -- /usr/bin/snap set aproxy proxy=squid.internal:3128
+/snap/bin/lxc exec runner -- sh -c 'nft -f - << EOF
+define default-ip = $(ip route get $(ip route show 0.0.0.0/0 | grep -oP 'via \K\S+') | grep -oP 'src \K\S+')
+define private-ips = { 10.0.0.0/8, 127.0.0.1/8, 172.16.0.0/12, 192.168.0.0/16 }
+table ip aproxy
+flush table ip aproxy
+table ip aproxy {
+      chain prerouting {
+              type nat hook prerouting priority dstnat; policy accept;
+              ip daddr != \$private-ips tcp dport { 80, 443 } counter dnat to \$default-ip:8443
+      }
+
+      chain output {
+              type nat hook output priority -100; policy accept;
+              ip daddr != \$private-ips tcp dport { 80, 443 } counter dnat to \$default-ip:8443
+      }
+}
+EOF'
 
 # Download and verify checksum of yq
 /snap/bin/lxc exec runner -- wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
