ca908073d803745c9eca1ab4084e08b9bd26f4fc
Christopher Bartz <christopher.bartz@canonical.com>
2023-11-17 11:36:53 +0100

    Create fixture

--- tests/integration/test_charm_with_proxy.py
+++ tests/integration/test_charm_with_proxy.py
@@ -3,6 +3,7 @@
 
 """Test the usage of a proxy server."""
 import pytest
+import pytest_asyncio
 from juju.application import Application
 from juju.model import Model
 
@@ -13,16 +14,9 @@ from tests.integration.helpers import (
 )
 
 
-@pytest.mark.asyncio
-@pytest.mark.abort_on_fail
-async def test_usage_of_aproxy(model: Model, app_no_runner: Application, squid_proxy: str) -> None:
-    """
-    arrange: A working application with one runner using aproxy configured for a proxy server.
-    act: Run curl in the runner.
-    assert: The aproxy log contains the request.
-    """
-    app = app_no_runner  # Rename to make it clear that the app will contain a runner.
-    unit = app.units[0]
+@pytest_asyncio.fixture(scope="module", name="app_with_aproxy")
+async def app_with_aproxy_fixture(model: Model, app_no_runner: Application, squid_proxy: str) -> Application:
+    """Application configured to use aproxy"""
 
     await model.set_config(
         {
@@ -33,9 +27,21 @@ async def test_usage_of_aproxy(model: Model, app_no_runner: Application, squid_p
         }
     )
 
-    await app.set_config({"use-aproxy": "true"})
-    await ensure_charm_has_runner(app, model)
+    await app_no_runner.set_config({"use-aproxy": "true"})
 
+    return app_no_runner
+
+
+@pytest.mark.asyncio
+@pytest.mark.abort_on_fail
+async def test_usage_of_aproxy(model: Model, app_with_aproxy: Application) -> None:
+    """
+    arrange: A working application with one runner using aproxy configured for a proxy server.
+    act: Run curl in the runner.
+    assert: The aproxy log contains the request.
+    """
+    await ensure_charm_has_runner(app_with_aproxy, model)
+    unit = app_with_aproxy.units[0]
     names = await get_runner_names(unit)
     assert names
     runner_name = names[0]
