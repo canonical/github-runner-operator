50f7564eaa292cd4323a6253de5defb7cb44dc4c
charlie4284 <yangsoo.yoon@canonical.com>
2024-01-17 13:34:47 +0800

    docs: add docstring to testing option values

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,11 +9,18 @@ from pytest import Parser
 
 def pytest_addoption(parser: Parser):
     """Add options to pytest parser."""
+    # The path to repository in <org>/<repo> or <user>/<repo> format.
     parser.addoption("--path", action="store")
+    # The GitHub Personal Access Token.
     parser.addoption("--token", action="store")
+    # The prebuilt github-runner-operator charm file.
     parser.addoption("--charm-file", action="store")
+    # An alternative token to test repo-policy-compliance.
     parser.addoption("--token-alt", action="store")
+    # HTTP proxy configuration value for juju model proxy configuration.
     parser.addoption("--http-proxy", action="store")
+    # HTTPS proxy configuration value for juju model proxy configuration.
     parser.addoption("--https-proxy", action="store")
+    # No proxy configuration value for juju model proxy configuration.
     parser.addoption("--no-proxy", action="store")
     parser.addoption("--loop-device", action="store")
