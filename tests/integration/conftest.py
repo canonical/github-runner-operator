13ebc683fb8cc64dbb2fe2e0f7a59d245c67c3f7
charlie4284 <yangsoo.yoon@canonical.com>
2024-01-22 12:51:07 +0800

    fix: no tokens csv

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -97,7 +97,7 @@ def token(pytestconfig: pytest.Config) -> str:
     token = pytestconfig.getoption("--token")
     tokens_csv = pytestconfig.getoption("--tokens")
     assert token or tokens_csv, "Please specify the --token or --tokens command line option"
-    tokens = {token.strip() for token in (tokens_csv.split(",") if tokens_csv != "" else [])}
+    tokens = {token.strip() for token in (tokens_csv.split(",") if tokens_csv else [])}
     tokens.add(token)
     random_token = random.choice(list(tokens))
     return random_token
