d061298751b65b3c832f78403886e292982038ba
charlie4284 <yangsoo.yoon@canonical.com>
2024-01-22 09:57:10 +0800

    docs: parser help messages

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,18 +9,35 @@ from pytest import Parser
 
 def pytest_addoption(parser: Parser):
     """Add options to pytest parser."""
-    # The path to repository in <org>/<repo> or <user>/<repo> format.
-    parser.addoption("--path", action="store")
-    # The GitHub Personal Access Token.
-    parser.addoption("--token", action="store")
-    # The prebuilt github-runner-operator charm file.
-    parser.addoption("--charm-file", action="store")
-    # An alternative token to test repo-policy-compliance.
-    parser.addoption("--token-alt", action="store")
-    # HTTP proxy configuration value for juju model proxy configuration.
-    parser.addoption("--http-proxy", action="store")
-    # HTTPS proxy configuration value for juju model proxy configuration.
-    parser.addoption("--https-proxy", action="store")
-    # No proxy configuration value for juju model proxy configuration.
-    parser.addoption("--no-proxy", action="store")
-    parser.addoption("--loop-device", action="store")
+    parser.addoption(
+        "--path",
+        action="store",
+        help="The path to repository in <org>/<repo> or <user>/<repo> format.",
+    )
+    parser.addoption("--token", action="store", help="The GitHub Personal Access Token.")
+    parser.addoption(
+        "--charm-file", action="store", help="The prebuilt github-runner-operator charm file."
+    )
+    parser.addoption(
+        "--token-alt", action="store", help="An alternative token to test repo-policy-compliance."
+    )
+    parser.addoption(
+        "--http-proxy",
+        action="store",
+        help="HTTP proxy configuration value for juju model proxy configuration.",
+    )
+    parser.addoption(
+        "--https-proxy",
+        action="store",
+        help="HTTPS proxy configuration value for juju model proxy configuration.",
+    )
+    parser.addoption(
+        "--no-proxy",
+        action="store",
+        help="No proxy configuration value for juju model proxy configuration.",
+    )
+    parser.addoption(
+        "--loop-device",
+        action="store",
+        help="The loop device to create shared FS for metrics logging",
+    )
