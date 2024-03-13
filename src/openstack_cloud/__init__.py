3ee927fe65b234c2c5a3f8bc2740ec0e4fbd9047
charlie4284 <yangsoo.yoon@canonical.com>
2024-03-13 09:25:37 +0800

    add cloudconfig type

--- src/openstack_cloud/__init__.py
+++ src/openstack_cloud/__init__.py
@@ -4,6 +4,7 @@
 """Module for managing Openstack cloud."""
 
 from pathlib import Path
+from typing import TypedDict, cast
 
 import yaml
 
@@ -12,7 +13,17 @@ from errors import OpenStackInvalidConfigError
 CLOUDS_YAML_PATH = Path(Path.home() / ".config/openstack/clouds.yaml")
 
 
-def _validate_cloud_config(cloud_config: dict) -> None:
+class CloudConfig(TypedDict):
+    """The parsed clouds.yaml configuration dictionary.
+
+    Attributes:
+        clouds: A mapping of key "clouds" to cloud name mapped to cloud configuration.
+    """
+
+    clouds: dict[str, dict]
+
+
+def _validate_cloud_config(cloud_config: dict) -> CloudConfig:
     """Validate the format of the cloud configuration.
 
     Args:
@@ -26,12 +37,12 @@ def _validate_cloud_config(cloud_config: dict) -> None:
         clouds = list(cloud_config["clouds"].keys())
     except KeyError as exc:
         raise OpenStackInvalidConfigError("Missing key 'clouds' from config.") from exc
-
     if not clouds:
         raise OpenStackInvalidConfigError("No clouds defined in clouds.yaml.")
+    return cast(CloudConfig, cloud_config)
 
 
-def _write_config_to_disk(cloud_config: dict) -> None:
+def _write_config_to_disk(cloud_config: CloudConfig) -> None:
     """Write the cloud configuration to disk.
 
     Args:
@@ -52,5 +63,5 @@ def initialize(cloud_config: dict) -> None:
     Raises:
         InvalidConfigError: if the format of the config is invalid.
     """
-    _validate_cloud_config(cloud_config)
-    _write_config_to_disk(cloud_config)
+    valid_config = _validate_cloud_config(cloud_config)
+    _write_config_to_disk(valid_config)
