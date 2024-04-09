2609a2df3fdf69cbd8175a26b68ea37a5d769230
charlie4284 <yangsoo.yoon@canonical.com>
2024-04-09 14:05:50 +0800

    add retry (builder lxd not ready error)

--- src/runner_manager.py
+++ src/runner_manager.py
@@ -786,6 +786,7 @@ class RunnerManager:
             cmd += ["test"]
         return cmd
 
+    @retry(tries=3, delay=30, local_logger=logger)
     def build_runner_image(self) -> None:
         """Build the LXD image for hosting runner.
 
