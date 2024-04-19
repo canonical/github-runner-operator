5251d5d3396a2c7c8af62a0c1323e3dc447066b0
Christopher Bartz <christopher.bartz@canonical.com>
2024-04-19 11:29:15 +0200

    Define storage manager interface

---
+++ src/metrics/storage.py
@@ -0,0 +1,38 @@
+#  Copyright 2024 Canonical Ltd.
+#  See LICENSE file for licensing details.
+
+"""Classes and functions defining the metrics storage."""
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Protocol, Iterator, Callable
+
+
+@dataclass
+class MetricsStorage:
+    """Storage for the metrics.
+
+    Attributes:
+        path: The path to the directory holding the metrics inside the charm.
+        runner_name: The name of the associated runner.
+    """
+
+    path: Path
+    runner_name: str
+
+
+class StorageManager(Protocol):
+    """Storage manager interface
+
+    Attributes:
+        create: Method to create a new storage. Returns the created storage.
+          Raises an exception CreateMetricsStorageError if the storage already exists.
+        list_all: Method to list all storages.
+        get: Method to get a storage by name.
+        delete: Method to delete a storage by name.
+        move_to_quarantine: Method to move a storage to quarantine.
+    """
+    create: Callable[[str], MetricsStorage]
+    list_all: Callable[[], Iterator[MetricsStorage]]
+    get: Callable[[str], MetricsStorage]
+    delete: Callable[[str], None]
+    move_to_quarantine: Callable[[str], None]
