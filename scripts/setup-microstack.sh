e09913c533628c8a0dc26c5a5cd747cd0277fa1f
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2024-04-26 10:50:42 +0800

    Allow micostack external_network to be access by host

--- scripts/setup-microstack.sh
+++ scripts/setup-microstack.sh
@@ -33,14 +33,82 @@ retry() {
 }
 # microk8s charm installed by microstack tries to create an alias for kubectl and fails otherwise
 sudo snap remove kubectl
-# Install microstack
+
+# Configure and install microstack
+sudo sed -i 's/microstack/microstack.multipass/g' /etc/hosts
+
+cat <<EOF > preseed.yaml
+bootstrap:
+  # Management networks shared by hosts (CIDRs, separated by comma)
+  management_cidr: 10.232.48.0/24
+addons:
+  # MetalLB address allocation range (supports multiple ranges, comma separated)
+  metallb: 10.20.21.10-10.20.21.20
+user:
+  # Populate OpenStack cloud with demo user, default images, flavors etc
+  run_demo_setup: True
+  # Username to use for access to OpenStack
+  username: demo
+  # Password to use for access to OpenStack
+  password: QNBy8XQqV3Lu
+  # Network range to use for project network
+  cidr: 192.168.122.0/24
+  # List of nameservers guests should use for DNS resolution
+  nameservers: 10.232.48.1
+  # Enable ping and SSH access to instances?
+  security_group_rules: True
+  # Local or remote access to VMs
+  remote_access_location: local
+# # Local Access
+# external_network:
+#   # CIDR of OpenStack external network - arbitrary but must not be in use
+#   cidr: 10.20.20.0/24
+#   # Start of IP allocation range for external network
+#   start: 10.20.20.2
+#   # End of IP allocation range for external network
+#   end: 10.20.20.254
+#   # Network type for access to external network
+#   network_type: flat
+#   # VLAN ID to use for external network
+#   segmentation_id:
+# Remote Access
+external_network:
+  # CIDR of network to use for external networking
+  cidr: 10.20.20.0/24
+  # IP address of default gateway for external network
+  gateway: 10.20.20.1
+  # Start of IP allocation range for external network
+  start: 10.20.20.2
+  # End of IP allocation range for external network
+  end: 10.20.20.254
+  # Network type for access to external network
+  network_type: flat
+  # VLAN ID to use for external network
+  segmentation_id:
+  # Free network interface that will be configured for external traffic
+  nic: $(ip -o -4 route show to default | awk '{print $5}')
+# MicroCeph config
+microceph_config:
+  microstack.multipass:
+    # Disks to attach to MicroCeph
+    osd_devices:
+EOF
+
 sudo snap install openstack --channel 2023.1 --devmode
 timeout 180 sunbeam prepare-node-script | bash -x
+<<<<<<< Updated upstream
 sleep 10
 # The following can takes around 30 mins..., set timeout 60 mins.
 timeout 3600 sudo -g snap_daemon sunbeam cluster bootstrap --accept-defaults
 # 2024/03/11 Demo user setup should be removed after openstack server creation PR.
 retry 'sudo -g snap_daemon sunbeam configure --accept-defaults --openrc demo-openrc' 'Configuring sunbeam cluster' 1
+=======
+sudo usermod -a -G snap_daemon $USER
+# The following can takes around 30 mins..., set timeout 45 mins.
+timeout 2700 sudo -g snap_daemon sunbeam cluster bootstrap -p preseed.yaml
+sudo -g snap_daemon sunbeam configure -p preseed.yaml --openrc demo-openrc
+# The microstack should be running now.
+>>>>>>> Stashed changes
 clouds_yaml="${PWD}/clouds.yaml"
 # Use admin account for access to create flavors, etc.
 sg snap_daemon -c "sunbeam cloud-config --admin" | tee "$clouds_yaml"
