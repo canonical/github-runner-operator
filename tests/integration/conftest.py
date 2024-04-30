33fec5f6b374c64938d84355ecd1e140aa527bed
charlie4284 <yangsoo.yoon@canonical.com>
2024-04-30 10:42:07 +0800

    grafana agent revision pin

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -543,7 +543,7 @@ async def app_with_grafana_agent_integrated_fixture(
     model: Model, app_no_runner: Application
 ) -> AsyncIterator[Application]:
     """Setup the charm to be integrated with grafana-agent using the cos-agent integration."""
-    grafana_agent = await model.deploy("grafana-agent", channel="latest/edge")
+    grafana_agent = await model.deploy("grafana-agent", channel="latest/edge", revision=105)
     await model.relate(f"{app_no_runner.name}:cos-agent", f"{grafana_agent.name}:cos-agent")
     await model.wait_for_idle(apps=[app_no_runner.name], status=ACTIVE)
     await model.wait_for_idle(apps=[grafana_agent.name])
