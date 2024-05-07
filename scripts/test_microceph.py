a94a3029dd528613da7e30c4e0b5ecc39674774a
Andrew Liaw <43424755+yhaliaw@users.noreply.github.com>
2024-05-07 19:00:15 +0800

    Wait before

--- scripts/test_microceph.py
+++ scripts/test_microceph.py
@@ -7,6 +7,8 @@ import boto3
 
 
 if __name__ == "__main__":
+    print("Wait for 60 seconds")
+    sleep(60)
     print("Setting up microceph")
     subprocess.run(["sudo", "snap", "install", "microceph"], check=True)
     subprocess.run(["sudo", "microceph", "cluster", "bootstrap"], check=True)
@@ -30,8 +32,6 @@ if __name__ == "__main__":
     key = json.loads(output)["keys"][0]
     key_id = key["access_key"]
     secret_key = key["secret_key"]
-    print("Wait for 60 seconds")
-    sleep(60)
     print("Creating microceph bucket")
     boto3.client(
         "s3",
