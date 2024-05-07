8f6a40c47c88dbde10d35fc5a5d931120d4200ec
Andrew Liaw <43424755+yhaliaw@users.noreply.github.com>
2024-05-07 18:30:04 +0800

    Test microceph

---
+++ scripts/test_microceph.py
@@ -0,0 +1,40 @@
+import subprocess
+import logging
+import json
+
+import boto3
+
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    logger.info("Setting up microceph")
+    subprocess.run(["sudo", "snap", "install", "microceph"], check=True)
+    subprocess.run(["sudo", "microceph", "cluster", "bootstrap"], check=True)
+    subprocess.run(["sudo", "microceph", "disk", "add", "loop,4G,3"], check=True)
+    subprocess.run(["sudo", "microceph", "enable", "rgw"], check=True)
+    output = subprocess.run(
+        [
+            "sudo",
+            "microceph.radosgw-admin",
+            "user",
+            "create",
+            "--uid",
+            "test",
+            "--display-name",
+            "test",
+        ],
+        capture_output=True,
+        check=True,
+        encoding="utf-8",
+    ).stdout
+    key = json.loads(output)["keys"][0]
+    key_id = key["access_key"]
+    secret_key = key["secret_key"]
+    logger.info("Creating microceph bucket")
+    boto3.client(
+        "s3",
+        endpoint_url="http://localhost",
+        aws_access_key_id=key_id,
+        aws_secret_access_key=secret_key,
+    ).create_bucket(Bucket="test")
+    logger.info("Set up microceph")
