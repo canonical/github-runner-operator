e80d941b440fd73f4ef70253a699ff607db95c46
Andrew Liaw <43424755+yhaliaw@users.noreply.github.com>
2024-05-14 09:26:59 +0200

    Improve the test logging

--- scripts/test_hardware.sh
+++ scripts/test_hardware.sh
@@ -1,8 +1,11 @@
 
 while  true
 do
-    echo 'Start end'
+    echo 'Start test'
     date
+    echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
     curl -sIXGET https://github.com
+    echo '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+    echo 'End test'
     sleep 60
 done
