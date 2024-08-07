dd14f44895f5d870c4ef150d73b0121bd3b543ae
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2024-08-07 17:00:19 +0800

    Adding the docstring for github_runner_manager

--- src/manager/github_runner_manager.py
+++ src/manager/github_runner_manager.py
@@ -56,6 +56,14 @@ class GithubRunnerManager:
     def get_runners(
         self, states: Sequence[GithubRunnerState] | None = None
     ) -> tuple[SelfHostedRunner]:
+        """Get info on self-hosted runners of certain states.
+
+        Args:
+            states: Filter the runners for these states. If None, all runners are returned.
+        
+        Returns:
+            Information on the runners.
+        """
         runner_list = self._github.get_runner_github_info(self._path)
         return tuple(
             runner
@@ -65,20 +73,50 @@ class GithubRunnerManager:
         )
 
     def delete_runners(self, states: Sequence[GithubRunnerState] | None = None) -> None:
+        """Delete the self-hosted runners of certain states.
+        
+        Args:
+            states: Filter the runners for these states. If None, all runners are deleted.
+            
+        Returns:
+            Information on the runners.
+        """
         runner_list = self.get_runners(states)
         for runner in runner_list:
             self._github.delete_runner(self._path, runner.id)
 
     def get_registration_token(self) -> str:
+        """Get registration token from GitHub.
+
+        This token is used for registering self-hosted runners.
+
+        Returns:
+            The registration token.
+        """
         return self._github.get_runner_registration_token(self._path)
 
     def get_removal_token(self) -> str:
+        """Get removal token from GitHub.
+
+        This token is used for removing self-hosted runners.
+
+        Returns:
+            The removal token.
+        """
         return self._github.get_runner_remove_token(self._path)
 
     @staticmethod
     def _filter_runner_state(
         runner: SelfHostedRunner, states: Sequence[GithubRunnerState] | None
     ) -> bool:
+        """Filter the runner by the state.
+
+        Args:
+            states: Filter the runners for these states. If None, return true.
+
+        Returns:
+            True if the runner is in one of the state, else false.            
+        """
         if states is None:
             return True
         return GithubRunnerState.from_runner(runner) in states
