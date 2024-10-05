63d74f9c763345312442f1d75e9bbb954a4b453f
Christopher Bartz <christopher.bartz@canonical.com>
2024-10-05 07:48:38 +0200

    remove unnecessary and invalid wait_for

--- tests/integration/test_charm_base_image.py
+++ tests/integration/test_charm_base_image.py
@@ -48,7 +48,7 @@ async def test_runner_base_image(
     assert "noble" in str(stdout)
 
     # Workflow completes successfully
-    workflow_run = await dispatch_workflow(
+    await dispatch_workflow(
         app=app_no_wait,
         branch=test_github_branch,
         github_repository=github_repository,
@@ -56,4 +56,3 @@ async def test_runner_base_image(
         workflow_id_or_name=DISPATCH_E2E_TEST_RUN_WORKFLOW_FILENAME,
         dispatch_input={"runner-tag": app_no_wait.name, "runner-virt-type": "lxd"},
     )
-    await wait_for(lambda: workflow_run.update() and workflow_run.status == "completed", timeout=600)
