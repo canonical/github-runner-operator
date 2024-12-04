29378dcd9a42bfcb1209bfd82d83dc6fdf25470e
Christopher Bartz <christopher.bartz@canonical.com>
2024-12-04 11:31:18 +0100

    make unit test more readable

--- github-runner-manager/tests/unit/openstack_cloud/test_openstack_runner_manager.py
+++ github-runner-manager/tests/unit/openstack_cloud/test_openstack_runner_manager.py
@@ -18,12 +18,50 @@ from github_runner_manager.openstack_cloud import (
     openstack_cloud,
     openstack_runner_manager,
 )
+from github_runner_manager.openstack_cloud.openstack_cloud import OpenstackCloud
 from github_runner_manager.openstack_cloud.openstack_runner_manager import (
     OUTDATED_METRICS_STORAGE_IN_SECONDS,
     OpenStackRunnerManager,
     OpenStackRunnerManagerConfig,
 )
 from tests.unit.factories import openstack_factory
+
+OPENSTACK_INSTANCE_PREFIX = "test"
+
+
+@pytest.fixture(name="runner_manager")
+def openstack_runner_manager_fixture(monkeypatch: pytest.MonkeyPatch) -> OpenStackRunnerManager:
+    """Mock required dependencies/configs and return an OpenStackRunnerManager instance."""
+    monkeypatch.setattr(
+        "github_runner_manager.openstack_cloud.openstack_runner_manager.metrics_storage",
+        MagicMock(),
+    )
+    monkeypatch.setattr(
+        "github_runner_manager.openstack_cloud.openstack_runner_manager.OpenstackCloud",
+        MagicMock(),
+    )
+
+    service_config_mock = MagicMock(spec=SupportServiceConfig)
+    service_config_mock.proxy_config = None
+    config = OpenStackRunnerManagerConfig(
+        name="test",
+        prefix="test",
+        credentials=MagicMock(),
+        server_config=MagicMock(),
+        runner_config=MagicMock(),
+        service_config=service_config_mock,
+        system_user_config=MagicMock(),
+    )
+
+    return OpenStackRunnerManager(config=config)
+
+
+@pytest.fixture(name="runner_metrics_mock")
+def runner_metrics_mock_fixture(monkeypatch: pytest.MonkeyPatch) -> MagicMock:
+    """Mock the runner_metrics module."""
+    runner_metrics_mock = MagicMock(spec=runner)
+    monkeypatch.setattr(openstack_runner_manager, "runner_metrics", runner_metrics_mock)
+    return runner_metrics_mock
 
 
 @pytest.mark.parametrize(
@@ -96,6 +134,7 @@ def test__cleanup_extract_metrics(
     undecided_runner_storage: set[tuple[str, datetime]],
     expected_storage_to_be_extracted: set[str],
     monkeypatch: pytest.MonkeyPatch,
+    runner_metrics_mock: MagicMock,
 ):
     """
     arrange: Given different combinations of healthy, unhealthy and undecided runners.
@@ -103,8 +142,6 @@ def test__cleanup_extract_metrics(
     assert: runner_metrics.extract is called with the expected storage to be extracted.
     """
     metric_storage_manager = MagicMock(spec=StorageManager)
-    runner_metrics_mock = MagicMock(spec=runner)
-    monkeypatch.setattr(openstack_runner_manager, "runner_metrics", runner_metrics_mock)
     now = datetime.now()
     all_runner_name_metrics_storage = [
         _create_metrics_storage(runner_name, now)
@@ -129,105 +166,57 @@ def test__cleanup_extract_metrics(
 
 
 @pytest.mark.parametrize(
-    "healthy_count, unhealthy_count, undecided_count",
+    "healthy_count, unhealthy_count, unknown_count",
     [
         pytest.param(1, 1, 1, id="one of each"),
         pytest.param(2, 1, 1, id="two healthy"),
         pytest.param(1, 2, 1, id="two unhealthy"),
-        pytest.param(1, 1, 2, id="two undecided"),
+        pytest.param(1, 1, 2, id="two unknown"),
         pytest.param(0, 0, 0, id="no runners"),
-        pytest.param(0, 0, 1, id="one undecided"),
+        pytest.param(0, 0, 1, id="one unknown"),
         pytest.param(0, 1, 0, id="one unhealthy"),
         pytest.param(1, 0, 0, id="one healthy"),
     ],
 )
 def test_cleanup_ignores_runners_with_health_check_errors(
-    healthy_count: int, unhealthy_count: int, undecided_count: int, monkeypatch: pytest.MonkeyPatch
+    healthy_count: int,
+    unhealthy_count: int,
+    unknown_count,
+    monkeypatch: pytest.MonkeyPatch,
+    runner_manager: OpenStackRunnerManager,
+    runner_metrics_mock: MagicMock,
 ):
     """
-    arrange: Given a combination of healthy/unhealthy/undecided (with a health check error) runners.
+    arrange: Given a combination of healthy/unhealthy/unknown(with a health check error) runners.
     act: When the cleanup method is called.
-    assert: Only the unhealthy runners are deleted and got metrics extracted.
+    assert: Only the unhealthy runners are deleted and their metrics are extracted.
     """
-    # TODO: Try to see if something can move to a fixture or helper function as this is a lot of setup.
-
-    service_config_mock = MagicMock(spec=SupportServiceConfig)
-    service_config_mock.proxy_config = None
-    config = OpenStackRunnerManagerConfig(
-        name="test",
-        prefix="test",
-        credentials=MagicMock(),
-        server_config=MagicMock(),
-        runner_config=MagicMock(),
-        service_config=service_config_mock,
-        system_user_config=MagicMock(),
-    )
-    openstack_cloud_mock = MagicMock(spec=openstack_runner_manager.OpenstackCloud)
     names = [
         f"test-{status}{i}"
         for status, count in [
             ("healthy", healthy_count),
             ("unhealthy", unhealthy_count),
-            ("undecided", undecided_count),
+            ("unknown", unknown_count),
         ]
         for i in range(count)
     ]
-    prefix = "test-"
-    openstack_cloud_mock.get_instances.return_value = [
-        openstack_cloud.OpenstackInstance(
-            server=openstack_factory.ServerFactory(
-                status="ACTIVE",
-                name=name,
-            ),
-            prefix="test",
-        )
-        for name in names
-    ]
-    health_checks_mock = MagicMock(spec=health_checks)
-
-    def health_checks_side_effect(openstack_cloud, instance):
-        if instance.server_name.startswith("test-healthy"):
-            return True
-        if instance.server_name.startswith("test-unhealthy"):
-            return False
-        raise OpenstackHealthCheckError("Health check failed")
-
-    health_checks_mock.check_runner.side_effect = health_checks_side_effect
-    monkeypatch.setattr(
-        "github_runner_manager.openstack_cloud.openstack_runner_manager.OpenstackCloud",
-        MagicMock(),
-    )
-    monkeypatch.setattr(
-        "github_runner_manager.openstack_cloud.openstack_runner_manager.metrics_storage",
-        MagicMock(),
-    )
+    openstack_cloud_mock = _create_openstack_cloud_mock(names)
+    runner_manager._openstack_cloud = openstack_cloud_mock
+    health_checks_mock = _create_health_checks_mock()
     monkeypatch.setattr(
         "github_runner_manager.openstack_cloud.openstack_runner_manager.health_checks",
         health_checks_mock,
     )
-    runner_manager = OpenStackRunnerManager(config=config)
-    runner_manager._openstack_cloud = openstack_cloud_mock
-
-    metric_storage_manager = MagicMock(spec=StorageManager)
-    runner_metrics_mock = MagicMock(spec=runner)
-    monkeypatch.setattr(openstack_runner_manager, "runner_metrics", runner_metrics_mock)
-    now = datetime.now()
-    all_runner_name_metrics_storage = [
-        _create_metrics_storage(runner_name, now) for runner_name in names
-    ]
-    metric_storage_manager.list_all = (all_runner_name_metrics_storage).__iter__
-
     runner_manager.cleanup(secrets.token_hex(16))
 
     assert openstack_cloud_mock.delete_instance.call_count == unhealthy_count
     for name in names:
-        name_without_prefix = name[len(prefix) :]
-        if name_without_prefix.startswith("unhealthy"):
-            openstack_cloud_mock.delete_instance.assert_any_call(name_without_prefix)
-
+        instance_id = name[len(OPENSTACK_INSTANCE_PREFIX) + 1 :]
+        if instance_id.startswith("unhealthy"):
+            openstack_cloud_mock.delete_instance.assert_any_call(instance_id)
     assert runner_metrics_mock.extract.call_count == 1
     assert runner_metrics_mock.extract.call_args[1]["runners"] == {
-        names for names in names if names.startswith("test-unhealthy")
+        names for names in names if names.startswith(f"{OPENSTACK_INSTANCE_PREFIX}-unhealthy")
     }
 
 
@@ -248,3 +237,42 @@ def _create_metrics_storage(runner_name: str, mtime: datetime) -> MetricsStorage
     stat.st_mtime = mtime.timestamp()
     metrics_storage.path.stat = stat_mock
     return metrics_storage
+
+
+def _create_openstack_cloud_mock(server_names: list[str]) -> MagicMock:
+    """Create an OpenstackCloud mock which returns servers with a given list of server names."""
+    openstack_cloud_mock = MagicMock(spec=OpenstackCloud)
+    openstack_cloud_mock.get_instances.return_value = [
+        openstack_cloud.OpenstackInstance(
+            server=openstack_factory.ServerFactory(
+                status="ACTIVE",
+                name=name,
+            ),
+            prefix=OPENSTACK_INSTANCE_PREFIX,
+        )
+        for name in server_names
+    ]
+    return openstack_cloud_mock
+
+
+def _create_health_checks_mock() -> MagicMock:
+    """Create a health check mock that returns a boolean or raises an error.
+
+    The logic is that if the server name starts with "test-healthy" it returns True,
+    if it starts with "test-unhealthy" it returns False, and raises an error otherwise.
+    """
+    health_checks_mock = MagicMock(spec=health_checks)
+
+    def _health_checks_side_effect(openstack_cloud, instance):
+        """Mock side effect for the health_checks.check_runner method.
+
+        This implements the logic mentioned in the docstring above.
+        """
+        if instance.server_name.startswith("test-healthy"):
+            return True
+        if instance.server_name.startswith("test-unhealthy"):
+            return False
+        raise OpenstackHealthCheckError("Health check failed")
+
+    health_checks_mock.check_runner.side_effect = _health_checks_side_effect
+    return health_checks_mock
