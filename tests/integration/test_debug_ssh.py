591a92ca00841c2f522897559ec988817c638aad
Javier de la Puente <javier.delapuente@canonical.com>
2024-12-05 13:23:51 +0100

    try to fix test_debug_ssh

--- tests/integration/test_debug_ssh.py
+++ tests/integration/test_debug_ssh.py
@@ -3,6 +3,8 @@
 
 """Integration tests for github-runner charm with ssh-debug integration."""
 import logging
+import socket
+import subprocess
 
 import pytest
 from github.Branch import Branch
@@ -11,7 +13,7 @@ from juju.application import Application
 from juju.model import Model
 
 from charm_state import DENYLIST_CONFIG_NAME
-from tests.integration.helpers.common import dispatch_workflow, get_job_logs
+from tests.integration.helpers.common import InstanceHelper, dispatch_workflow, get_job_logs
 from tests.status_name import ACTIVE
 
 logger = logging.getLogger(__name__)
@@ -27,6 +29,7 @@ async def test_ssh_debug(
     github_repository: Repository,
     test_github_branch: Branch,
     tmate_ssh_server_unit_ip: str,
+    instance_helper: InstanceHelper,
 ):
     """
     arrange: given an integrated GitHub-Runner charm and tmate-ssh-server charm with a denylist \
@@ -34,6 +37,13 @@ async def test_ssh_debug(
     act: when canonical/action-tmate is triggered.
     assert: the ssh connection info from action-log and tmate-ssh-server matches.
     """
+    # As the tmate_ssh_server runs in lxd under this host, we need all the connection
+    # that arrive to this host to port 10022 to go to the tmate_ssh_server unit.
+    subprocess.run(["sudo", "bash", "-c", "echo 1 > /proc/sys/net/ipv4/ip_forward"], check=True)
+    # This maybe problematic if we run the test twice in the same machine.
+    dnat_command_in_test_machine = f"sudo iptables -t nat -A PREROUTING -p tcp --dport 10022 -j DNAT --to-destination {tmate_ssh_server_unit_ip}:10022"
+    subprocess.run(dnat_command_in_test_machine.split(), check=True)
+
     await app_no_wait_openstack.set_config(
         {
             DENYLIST_CONFIG_NAME: (
@@ -45,6 +55,17 @@ async def test_ssh_debug(
         }
     )
     await model.wait_for_idle(status=ACTIVE, timeout=60 * 120)
+
+    unit = app_no_wait_openstack.units[0]
+    # We need the runner to connect to the current machine, instead of the tmate_ssh_server unit,
+    # as the tmate_ssh_server is not routable.
+    this_host_ip = _get_current_ip()
+    dnat_comman_in_runner = f"sudo iptables -t nat -A OUTPUT -p tcp --dport 10022 -j DNAT --to-destination {this_host_ip}:10022"
+    ret_code, stdout, stderr = await instance_helper.run_in_instance(
+        unit,
+        dnat_comman_in_runner,
+        assert_on_failure=True,
+    )
 
     # trigger tmate action
     logger.info("Dispatching workflow_dispatch_ssh_debug.yaml workflow.")
@@ -64,3 +85,14 @@ async def test_ssh_debug(
     logger.info("Logs: %s", logs)
     assert tmate_ssh_server_unit_ip in logs, "Tmate ssh server IP not found in action logs."
     assert "10022" in logs, "Tmate ssh server connection port not found in action logs."
+
+
+def _get_current_ip():
+    """Get the IP for the current machine."""
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(("2.2.2.2", 1))
+        return s.getsockname()[0]
+    finally:
+        s.close()
