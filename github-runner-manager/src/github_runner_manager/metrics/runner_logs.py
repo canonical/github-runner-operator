e9ec55da3ab39309d4cebe5320e338e1f548979f
Christopher Bartz <christopher.bartz@canonical.com>
2025-01-08 07:51:20 +0100

    update copyright

--- github-runner-manager/src/github_runner_manager/metrics/runner_logs.py
+++ github-runner-manager/src/github_runner_manager/metrics/runner_logs.py
@@ -1,4 +1,4 @@
-#  Copyright 2024 Canonical Ltd.
+#  Copyright 2025 Canonical Ltd.
 #  See LICENSE file for licensing details.
 
 """Functions to pull and remove the logs of the crashed runners."""
--- github-runner-manager/src/github_runner_manager/openstack_cloud/constants.py
+++ github-runner-manager/src/github_runner_manager/openstack_cloud/constants.py
@@ -1,4 +1,4 @@
-#  Copyright 2024 Canonical Ltd.
+#  Copyright 2025 Canonical Ltd.
 #  See LICENSE file for licensing details.
 
 """Common constants for the Openstack cloud module."""
