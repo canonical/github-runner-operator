7674ad7e66d6d09691c1c39b8024655d963b2813
Javier de la Puente <javier.delapuente@canonical.com>
2025-02-05 15:55:54 +0100

    oops

--- github-runner-manager/src/github_runner_manager/openstack_cloud/openstack_runner_manager.py
+++ github-runner-manager/src/github_runner_manager/openstack_cloud/openstack_runner_manager.py
@@ -655,8 +655,8 @@ class OpenStackRunnerManager(CloudRunnerManager):
             )
             cloud_init_log_output_result = ssh_conn.run("cat /var/log/cloud-init-output.log", warn=True, timeout=60)
             logger.error("/var/log/cloud-init-output.log stdout: %s", cloud_init_log_output_result.stdout)
-            cloud_init_log_result = ssh_conn.run("cat /var/log/cloud-init.log")
-            logger.error("/var/log/cloud-init.log stdout: %s", cloud_init_log_result.stdout, warn=True, timeout=60)
+            cloud_init_log_result = ssh_conn.run("cat /var/log/cloud-init.log", warn=True, timeout=60)
+            logger.error("/var/log/cloud-init.log stdout: %s", cloud_init_log_result.stdout)
             raise RunnerStartError(f"Runner startup process not found on {instance.server_name}")
         # A short running job may have already completed and exited the runner, hence check the
         # condition via cloud-init status check.
