a7e5bd754cde7ce4a76ec2c685b47e9a4822ddc6
Javier de la Puente <javier.delapuente@canonical.com>
2025-02-12 14:25:12 +0100

    let's go for the future!

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -357,7 +357,7 @@ async def image_builder_fixture(
             "github-runner-image-builder",
             application_name=application_name,
             channel="latest/edge",
-            revision=55,
+            revision=58,
             config={
                 "build-interval": "12",
                 "revision-history-limit": "2",
