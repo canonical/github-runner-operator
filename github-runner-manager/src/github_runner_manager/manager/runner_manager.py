bab582d6904b95b5fc3812dd37bb797e43b43631
Javier de la Puente <javier.delapuente@canonical.com>
2025-02-25 11:27:49 +0100

    we need these labels

--- github-runner-manager/src/github_runner_manager/manager/runner_manager.py
+++ github-runner-manager/src/github_runner_manager/manager/runner_manager.py
@@ -121,7 +121,10 @@ class RunnerManager:
 
         # This is quite ugly. labels should not be hidden inside OpenStack configuration.
         # It will be refactor in the multiple image/flavor PRs.
+
         labels = self._cloud._config.runner_config.labels  # pylint: disable=protected-access
+        # TODO THIS LABELS ARE INVALID, THERE IS NO self-hosted NOR linux!! ADD THEM MANUALLY
+        labels += ["self-hosted", "linux"]
         create_runner_args = [
             RunnerManager._CreateRunnerArgs(self._cloud, self._github, labels) for _ in range(num)
         ]
