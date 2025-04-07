0d9c5834f6f103865bd9cfc710774cc96a754bab
Javier de la Puente <javier.delapuente@canonical.com>
2025-04-07 09:34:00 +0200

    remove job manager client. This will go in a following PR

--- github-runner-manager/tests/integration/conftest.py
+++ github-runner-manager/tests/integration/conftest.py
@@ -77,7 +77,6 @@ def config_file_fixture(tmp_path_factory, config: dict) -> Path:
 
 @pytest.fixture(name="install_app", scope="module")
 def install_app_fixture() -> None:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "./../jobmanager_client"])
     subprocess.check_call([sys.executable, "-m", "pip", "install", "."])
 
 
--- jobmanager_client/jobmanager_client/api/__init__.py
+++
@@ -1,5 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from jobmanager_client.api.default_api import DefaultApi
-
