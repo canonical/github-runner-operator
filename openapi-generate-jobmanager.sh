54d4ea2338eb759e6e2633b238bdaf3b134a8b9f
Javier de la Puente <javier.delapuente@canonical.com>
2025-04-10 14:53:11 +0200

    quotes in sh

--- openapi-generate-jobmanager.sh
+++ openapi-generate-jobmanager.sh
@@ -2,5 +2,5 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/openapi.yaml  --package-name jobmanager_client -g python-pydantic-v1 -o /local/jobmanager/client
+docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate -i /local/openapi.yaml  --package-name jobmanager_client -g python-pydantic-v1 -o /local/jobmanager/client
 sudo chown -R "$USER" ./jobmanager
