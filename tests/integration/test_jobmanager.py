fd9cb416ef3b1a0e895a13a68bb5d76931f92139
Javier de la Puente <javier.delapuente@canonical.com>
2025-04-11 12:45:54 +0200

    more info in here

--- tests/integration/test_jobmanager.py
+++ tests/integration/test_jobmanager.py
@@ -4,12 +4,16 @@
 """Testing for jobmanager platform."""
 
 import http.server
+import json
 import logging
+import socket
 import socketserver
 from typing import AsyncIterator, Optional
 
 import pytest
 import pytest_asyncio
+from github_runner_manager.reactive.consumer import JobDetails
+from jobmanager_client.models.job import Job
 from juju.application import Application
 from juju.model import Model
 from ops.model import ActiveStatus
@@ -21,6 +25,7 @@ from tests.integration.helpers.charm_metrics import (
 )
 from tests.integration.helpers.common import reconcile
 from tests.integration.utils_reactive import (
+    add_to_queue,
     assert_queue_is_empty,
     clear_queue,
     get_mongodb_uri,
@@ -41,9 +46,11 @@ async def app_for_jobmanager_fixture(
     image_builder: Application,
 ) -> Application:
     """Application for testing reactive jobmanager."""
-    # await image_builder.set_config(
-    #     {"script-url": "https://pastebin.canonical.com/p/qVZyGB6Cz7/"}
-    # )
+    await image_builder.set_config(
+        {
+            "script-url": "https://git.launchpad.net/job-manager/plain/scripts/post-image-build.sh?h=main"
+        }
+    )
 
     logger.info("app_for_jobmanager fixture")
     if not existing_app_suffix:
@@ -83,7 +90,12 @@ async def app_fixture(
     await reconcile(app_for_jobmanager, app_for_jobmanager.model)
 
 
-def test_jobmanager(monkeypatch: pytest.MonkeyPatch, app: Application):
+@pytest.mark.abort_on_fail
+async def test_jobmanager(
+    monkeypatch: pytest.MonkeyPatch,
+    app: Application,
+    ops_test: OpsTest,
+):
     """
     arrange: TODO.
     act: TODO.
@@ -91,9 +103,47 @@ def test_jobmanager(monkeypatch: pytest.MonkeyPatch, app: Application):
     """
     logger.info("Start of test_jobmanager test")
 
-    with TestServer(monkeypatch, ("", 8000)) as httpd:
-        httpd.handle_get_request("", "hello", timeout=5)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip_address = s.getsockname()[0]
+    logger.info("IP Address to use: %s", ip_address)
+    s.close()
 
+    port = 8000
+    base_url = f"http://{ip_address}:{port}/"
+
+    mongodb_uri = await get_mongodb_uri(ops_test, app)
+    labels = {app.name, "x64"}
+
+    job_id = 99
+    job_url = f"{base_url}/v1/jobs/{job_id}"
+
+    job = JobDetails(
+        labels=labels,
+        url=job_url,
+    )
+
+    with TestServer(monkeypatch, ("", 8000)) as httpd:
+        add_to_queue(
+            json.dumps(json.loads(job.json()) | {"ignored_noise": "foobar"}),
+            mongodb_uri,
+            app.name,
+        )
+
+        returned_job = Job()
+        httpd.handle_get_request(
+            f"/v1/jobs/{job_id}", json.dumps(returned_job.to_json()), timeout=5
+        )
+
+        # here the agent will v1/jobs/{job_id}/health
+
+        # here github-runner go also v1/jobs/{job_id}
+
+        # ...
+        # here I call the agent in 8080 to execute "ls"
+
+        # agent will listen in 8080 for jobs to execute.
+        # we need another security special group for jobmanager.
     assert True
 
 
@@ -119,18 +169,24 @@ class TestServer(socketserver.TCPServer):
         super().__init__(server_address, self.mock_handler, bind_and_activate)
 
     def handle_get_request(self, url, response, timeout=10):
-        """TODO."""  # noqa: DCO020, DCO060
+        """TODO."""  # noqa: DCO020, DCO060, DCO050
         self.timeout = timeout
+        handler_executed = False
 
         def _handler(handler):
             """TODO."""
+            nonlocal handler_executed
+            # TODO FAIL IR URL IS WRONG!
             handler.send_response(200, "OK")
             handler.end_headers()
             handler.wfile.write(response.encode("utf-8"))
+            handler_executed = True
 
         try:
             oldval = getattr(self.mock_handler, "do_GET")
             setattr(self.mock_handler, "do_GET", _handler)
             self.handle_request()
+            if not handler_executed:
+                raise AssertionError("GRR, not called.")
         finally:
             setattr(self.mock_handler, "do_GET", oldval)
