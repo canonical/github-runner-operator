d7bc2504a9cd3ff08b03b5dc283e9138a4e42739
Javier de la Puente <javier.delapuente@canonical.com>
2025-04-29 13:52:21 +0200

    Fix linting

--- tests/integration/helpers/charm_metrics.py
+++ tests/integration/helpers/charm_metrics.py
@@ -8,7 +8,6 @@ import json
 import logging
 from time import sleep
 
-import requests
 from github.Branch import Branch
 from github.GithubException import GithubException
 from github.Repository import Repository
