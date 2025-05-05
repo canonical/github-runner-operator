ae110afb6494a8f410bbd2ed1cb6745d24aff9ad
Christopher Bartz <christopher.bartz@canonical.com>
2025-05-05 09:29:12 +0200

    lint

--- tests/integration/test_jobmanager.py
+++ tests/integration/test_jobmanager.py
@@ -26,9 +26,10 @@ from pytest_operator.plugin import OpsTest
 
 from charm_state import (
     BASE_VIRTUAL_MACHINES_CONFIG_NAME,
+    EXPERIMENTAL_JOB_MANAGER_ONLY_TOKEN_VALUE,
     MAX_TOTAL_VIRTUAL_MACHINES_CONFIG_NAME,
     PATH_CONFIG_NAME,
-    TOKEN_CONFIG_NAME, EXPERIMENTAL_JOB_MANAGER_ONLY_TOKEN_VALUE,
+    TOKEN_CONFIG_NAME,
 )
 from tests.integration.helpers.charm_metrics import clear_metrics_log
 from tests.integration.helpers.common import reconcile, wait_for
