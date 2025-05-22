cd0cfb7e686f2a05e45d91e2ae686970c0197c66
Christopher Bartz <christopher.bartz@canonical.com>
2025-05-22 13:30:52 +0200

    outcomment other tests

--- tests/integration/test_charm_runner.py
+++ tests/integration/test_charm_runner.py
@@ -40,102 +40,102 @@ async def app_fixture(
     await reconcile(basic_app, basic_app.model)
 
 
-@pytest.mark.openstack
-@pytest.mark.asyncio
-@pytest.mark.abort_on_fail
-async def test_check_runner(app: Application, instance_helper: OpenStackInstanceHelper) -> None:
-    """
-    arrange: A working application with one runner.
-    act: Run check_runner action.
-    assert: Action returns result with one runner.
-    """
-    await instance_helper.ensure_charm_has_runner(app)
-
-    action = await app.units[0].run_action("check-runners")
-    await action.wait()
-
-    assert action.status == "completed"
-    assert action.results["online"] == "1"
-    assert action.results["offline"] == "0"
-    assert action.results["unknown"] == "0"
-
-
-@pytest.mark.openstack
-@pytest.mark.asyncio
-@pytest.mark.abort_on_fail
-async def test_flush_runner_and_resource_config(
-    app: Application,
-    github_repository: Repository,
-    test_github_branch: Branch,
-    instance_helper: OpenStackInstanceHelper,
-) -> None:
-    """
-    arrange: A working application with one runner.
-    act:
-        1. Run Check_runner action. Record the runner name for later.
-        2. Nothing.
-        3. Change the virtual machine resource configuration.
-        4. Run flush_runner action.
-        5. Dispatch a workflow to make runner busy and call flush_runner action.
-
-    assert:
-        1. One runner exists.
-        2. Check the resource matches the configuration.
-        3. The runner is not flushed since by default it flushes idle.
-
-    Test are combined to reduce number of runner spawned.
-    """
-    await instance_helper.ensure_charm_has_runner(app)
-
-    # 1.
-    action: Action = await app.units[0].run_action("check-runners")
-    await action.wait()
-
-    assert action.status == "completed"
-    assert action.results["online"] == "1"
-    assert action.results["offline"] == "0"
-    assert action.results["unknown"] == "0"
-
-    runner_names = action.results["runners"].split(", ")
-    assert len(runner_names) == 1
-
-    # 2.
-    action = await app.units[0].run_action("flush-runners")
-    await action.wait()
-
-    # There is a race condition in here. When deleting a runner in openstack, it can take
-    # a while to get the runner deleted and the "flush-runner" will not spawn a new runner.
-    # We may need to call flush-runners twice and wait in the middle until the openstack
-    # instance disappear.
-
-    action = await app.units[0].run_action("check-runners")
-    await action.wait()
-
-    assert action.status == "completed"
-    assert action.results["online"] == "1"
-    assert action.results["offline"] == "0"
-    assert action.results["unknown"] == "0"
-
-    new_runner_names = action.results["runners"].split(", ")
-    assert len(new_runner_names) == 1
-    assert new_runner_names[0] != runner_names[0]
-
-    # 3.
-    workflow = await dispatch_workflow(
-        app=app,
-        branch=test_github_branch,
-        github_repository=github_repository,
-        conclusion="success",
-        workflow_id_or_name=DISPATCH_WAIT_TEST_WORKFLOW_FILENAME,
-        dispatch_input={"runner": app.name, "minutes": "5"},
-        wait=False,
-    )
-    await wait_for(lambda: workflow.update() or workflow.status == "in_progress")
-    action = await app.units[0].run_action("flush-runners")
-    await action.wait()
-
-    assert action.status == "completed"
-    assert action.results["delta"]["virtual-machines"] == "0"
+# @pytest.mark.openstack
+# @pytest.mark.asyncio
+# @pytest.mark.abort_on_fail
+# async def test_check_runner(app: Application, instance_helper: OpenStackInstanceHelper) -> None:
+#     """
+#     arrange: A working application with one runner.
+#     act: Run check_runner action.
+#     assert: Action returns result with one runner.
+#     """
+#     await instance_helper.ensure_charm_has_runner(app)
+#
+#     action = await app.units[0].run_action("check-runners")
+#     await action.wait()
+#
+#     assert action.status == "completed"
+#     assert action.results["online"] == "1"
+#     assert action.results["offline"] == "0"
+#     assert action.results["unknown"] == "0"
+#
+#
+# @pytest.mark.openstack
+# @pytest.mark.asyncio
+# @pytest.mark.abort_on_fail
+# async def test_flush_runner_and_resource_config(
+#     app: Application,
+#     github_repository: Repository,
+#     test_github_branch: Branch,
+#     instance_helper: OpenStackInstanceHelper,
+# ) -> None:
+#     """
+#     arrange: A working application with one runner.
+#     act:
+#         1. Run Check_runner action. Record the runner name for later.
+#         2. Nothing.
+#         3. Change the virtual machine resource configuration.
+#         4. Run flush_runner action.
+#         5. Dispatch a workflow to make runner busy and call flush_runner action.
+#
+#     assert:
+#         1. One runner exists.
+#         2. Check the resource matches the configuration.
+#         3. The runner is not flushed since by default it flushes idle.
+#
+#     Test are combined to reduce number of runner spawned.
+#     """
+#     await instance_helper.ensure_charm_has_runner(app)
+#
+#     # 1.
+#     action: Action = await app.units[0].run_action("check-runners")
+#     await action.wait()
+#
+#     assert action.status == "completed"
+#     assert action.results["online"] == "1"
+#     assert action.results["offline"] == "0"
+#     assert action.results["unknown"] == "0"
+#
+#     runner_names = action.results["runners"].split(", ")
+#     assert len(runner_names) == 1
+#
+#     # 2.
+#     action = await app.units[0].run_action("flush-runners")
+#     await action.wait()
+#
+#     # There is a race condition in here. When deleting a runner in openstack, it can take
+#     # a while to get the runner deleted and the "flush-runner" will not spawn a new runner.
+#     # We may need to call flush-runners twice and wait in the middle until the openstack
+#     # instance disappear.
+#
+#     action = await app.units[0].run_action("check-runners")
+#     await action.wait()
+#
+#     assert action.status == "completed"
+#     assert action.results["online"] == "1"
+#     assert action.results["offline"] == "0"
+#     assert action.results["unknown"] == "0"
+#
+#     new_runner_names = action.results["runners"].split(", ")
+#     assert len(new_runner_names) == 1
+#     assert new_runner_names[0] != runner_names[0]
+#
+#     # 3.
+#     workflow = await dispatch_workflow(
+#         app=app,
+#         branch=test_github_branch,
+#         github_repository=github_repository,
+#         conclusion="success",
+#         workflow_id_or_name=DISPATCH_WAIT_TEST_WORKFLOW_FILENAME,
+#         dispatch_input={"runner": app.name, "minutes": "5"},
+#         wait=False,
+#     )
+#     await wait_for(lambda: workflow.update() or workflow.status == "in_progress")
+#     action = await app.units[0].run_action("flush-runners")
+#     await action.wait()
+#
+#     assert action.status == "completed"
+#     assert action.results["delta"]["virtual-machines"] == "0"
 
 
 @pytest.mark.openstack
