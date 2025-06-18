4221027eced204ff8c6ca248d189004b7440e124
charlie4284 <yangsoo.yoon@canonical.com>
2025-06-18 06:02:14 +0000

    feat: add initial reconcile metrics

--- github-runner-manager/src/github_runner_manager/metrics/__init__.py
+++ github-runner-manager/src/github_runner_manager/metrics/__init__.py
@@ -2,3 +2,7 @@
 #  See LICENSE file for licensing details.
 
 """Package for common metrics-related code."""
+
+from .reconcile import *
+
+__all__ = reconcile.__all__
--- github-runner-manager/src/github_runner_manager/metrics/reconcile.py
+++ github-runner-manager/src/github_runner_manager/metrics/reconcile.py
@@ -3,9 +3,30 @@
 
 """Module for collecting metrics related to the reconciliation process."""
 
-from prometheus_client import Histogram
+from prometheus_client import Gauge, Histogram
 
 RECONCILE_DURATION_SECONDS = Histogram(
     name="reconcile_duration_seconds",
     documentation="Duration of reconciliation (seconds)",
+    labelnames=["flavor"],
+)
+EXPECTED_RUNNERS_COUNT = Gauge(
+    name="expected_runners_count",
+    documentation="Expected number of runners",
+    labelnames=["flavor"],
+)
+BUSY_RUNNERS_COUNT = Gauge(
+    name="busy_runners_count",
+    documentation="Number of busy runners",
+    labelnames=["flavor"],
+)
+IDLE_RUNNERS_COUNT = Gauge(
+    name="idle_runners_count",
+    documentation="Number of idle runners",
+    labelnames=["flavor"],
+)
+CLEANED_RUNNERS_TOTAL = Gauge(
+    name="cleaned_runners_total",
+    documentation="Total number of runners cleaned up",
+    labelnames=["flavor"],
 )
