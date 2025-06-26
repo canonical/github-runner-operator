09fc69a4e5399bb204f28b37f380a8eba9434d4d
charlie4284 <yangsoo.yoon@canonical.com>
2025-06-26 06:25:27 +0000

    test: add prometheus datasource

--- tests/integration/test_prometheus_metrics.py
+++ tests/integration/test_prometheus_metrics.py
@@ -9,6 +9,7 @@ from typing import Generator, cast
 import jubilant
 import pytest
 import pytest_asyncio
+from jubilant.statustypes import AppStatus
 from juju.application import Application
 
 logger = logging.getLogger(__name__)
@@ -27,10 +28,28 @@ def k8s_juju_fixture(
 @pytest.fixture(scope="module", name="prometheus_app")
 def prometheus_app_fixture(k8s_juju: jubilant.Juju):
     """Deploy prometheus charm."""
-    k8s_juju.deploy("grafana-k8s", channel="1/stable")
-    k8s_juju.wait(lambda status: jubilant.all_active(status, "grafana-k8s"))
-    k8s_juju.offer("grafana-k8s", endpoint="grafana-dashboards-provider")
+    k8s_juju.deploy("prometheus-k8s", channel="1/stable")
+    k8s_juju.wait(lambda status: jubilant.all_active(status, "prometheus-k8s"))
+    k8s_juju.offer("prometheus-k8s", endpoint="receive-remote-write")
     return k8s_juju.status().apps["prometheus-k8s"]
+
+
+@pytest.fixture(scope="module", name="grafana_app")
+def grafana_app_fixture(k8s_juju: jubilant.Juju):
+    """Deploy prometheus charm."""
+    k8s_juju.deploy("grafana-k8s", channel="1/stable")
+    k8s_juju.integrate("grafana-k8s:grafana-source", "prometheus-k8s:grafana-source")
+    k8s_juju.wait(lambda status: jubilant.all_active(status, "grafana-k8s", "prometheus-k8s"))
+    k8s_juju.offer("grafana-k8s", endpoint="grafana-dashboard")
+    return k8s_juju.status().apps["grafana-k8s"]
+
+
+@pytest.fixture(scope="module", name="grafana_password")
+def grafana_password_fixture(k8s_juju: jubilant.Juju, grafana_app: AppStatus):
+    """Get Grafana dashboard password."""
+    unit = next(iter(grafana_app.units.keys()))
+    result = k8s_juju.run(unit, "get-admin-password")
+    return result.results["admin-password"]
 
 
 @pytest.fixture(scope="module", name="openstack_app_cos_agent")
@@ -46,16 +65,27 @@ def openstack_app_cos_agent_fixture(juju: jubilant.Juju, app_openstack_runner: A
 
 @pytest.mark.openstack
 def test_prometheus_metrics(
-    juju: jubilant.Juju, k8s_juju: jubilant.Juju, openstack_app_cos_agent: Application
+    juju: jubilant.Juju,
+    k8s_juju: jubilant.Juju,
+    openstack_app_cos_agent: Application,
+    grafana_app: AppStatus,
+    grafana_password: str,
 ):
     """
     arrange: given a prometheus charm application.
     act: when GitHub runner is integrated.
     assert: the datasource is registered and basic metrics are available.
     """
-    offer_name = "grafana-k8s"
-    juju.cli(f"consume microk8s:{k8s_juju.model}.{offer_name}")
-    juju.integrate(openstack_app_cos_agent.name, offer_name)
-    juju.wait(lambda status: jubilant.all_agents_idle(status, openstack_app_cos_agent.name))
-    grafana_ip = k8s_juju.status().apps["grafana-k8s"].units["grafana-k8s/0"].address
-    assert False, grafana_ip
+    prometheus_offer_name = "prometheus-k8s"
+    grafana_offer_name = "grafana-k8s"
+    juju.cli(f"consume microk8s:{k8s_juju.model}.{prometheus_offer_name}")
+    juju.cli(f"consume microk8s:{k8s_juju.model}.{grafana_offer_name}")
+    juju.integrate("grafana-agent", prometheus_offer_name)
+    juju.integrate("grafana-agent", grafana_offer_name)
+    juju.wait(
+        lambda status: jubilant.all_agents_idle(
+            status, openstack_app_cos_agent.name, "grafana-agent"
+        )
+    )
+    grafana_ip = grafana_app.units["grafana-k8s/0"].address
+    assert False, f"admin:{grafana_password}@{grafana_ip}:3000/"
