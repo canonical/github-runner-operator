a5ef508c1ddfe14a6d89ba85a0798df4f1f199fc
Christopher Bartz <christopher.bartz@canonical.com>
2025-07-03 13:02:46 +0200

    create api client on each request

--- github-runner-manager/tests/unit/test_jobmanager_api.py
+++ github-runner-manager/tests/unit/test_jobmanager_api.py
@@ -39,67 +39,72 @@ def url_fixture():
 @pytest.fixture(name="jobmanager_api")
 def jobmanager_api(token, url):
     jobmanager_api = JobManagerAPI(token=token, url=url)
-    api_client = MagicMock()
-    jobmanager_api._api_client = api_client
-    jobmanager_api._runners_api = MagicMock()
-    jobmanager_api._jobs_api = MagicMock()
-    jobmanager_api._runners_api.api_client = jobmanager_api._jobs_api.api_client = api_client
     return jobmanager_api
 
 
-def test_jobmanager_api_initialize(token, url):
-    """
-    arrange: Arrange a test token and URL.
-    act: Created jobmanager api object.
-    assert: Default headers of underlying client are set to bearer authorization and url is set.
-    """
-    jobmanager_api = JobManagerAPI(token=token, url=url)
-    assert "Authorization" in jobmanager_api._api_client.default_headers
-    assert jobmanager_api._api_client.default_headers["Authorization"] == f"Bearer {token}"
-    assert jobmanager_api._api_client.configuration.host == url
-    assert jobmanager_api._jobs_api.api_client == jobmanager_api._api_client
-    assert jobmanager_api._runners_api.api_client == jobmanager_api._api_client
-
-
-def test_jobmanager_close(jobmanager_api):
+def test_jobmanager_api_sets_token_in_default_headers(token: str, url: str):
     """
     arrange: Create a jobmanager api object.
-    act: Call the JobManagerAPI close method
-    assert: The api_client's close method is called
+    act: Check if the generated api client has the token in default headers.
+    assert: The default headers contain the token.
     """
-    jobmanager_api.close()
+    jobmanager_api = JobManagerAPI(token=token, url=url)
 
-    jobmanager_api._api_client.close.assert_called_once()
+    api_client = jobmanager_api._create_api_client()
+    assert "Authorization" in api_client.default_headers
+    assert api_client.default_headers["Authorization"] == f"Bearer {token}"
 
 
-def test_jobmanager_api_get_runner_health(jobmanager_api):
+def test_jobmanager_api_sets_url_in_configuration(token: str, url: str):
+    """
+    arrange: Create a jobmanager api object.
+    act: Check if the generated api client has the url in configuration.
+    assert: The configuration host is set to the provided url.
+    """
+    jobmanager_api = JobManagerAPI(token=token, url=url)
+
+    api_client = jobmanager_api._create_api_client()
+    assert api_client.configuration.host == url
+
+
+def test_jobmanager_api_get_runner_health(
+    monkeypatch: pytest.MonkeyPatch, jobmanager_api: JobManagerAPI
+):
     """
     arrange: Create a jobmanager api object and stub the api client method.
     act: Call get_runner_health method.
     assert: The method returns a RunnerHealth object.
     """
-    jobmanager_api._runners_api.get_runner_health_v1_runners_runner_id_health_get = MagicMock(
-        return_value=RunnerHealthResponse(
-            label="label",
-            status=RunnerStatus.IN_PROGRESS.value,
-            deletable=True,
-            cpu_usage="10%",
-            ram_usage="20%",
-            disk_usage="30%",
+    monkeypatch.setattr(JobManagerAPI, "_create_api_client", MagicMock())
+    monkeypatch.setattr(
+        "jobmanager_client.RunnersApi.get_runner_health_v1_runners_runner_id_health_get",
+        MagicMock(
+            return_value=RunnerHealthResponse(
+                label="label",
+                status=RunnerStatus.IN_PROGRESS.value,
+                deletable=True,
+                cpu_usage="10%",
+                ram_usage="20%",
+                disk_usage="30%",
+            ),
         ),
     )
     response = jobmanager_api.get_runner_health(runner_id=123)
     assert RunnerHealth(status=RunnerStatus.IN_PROGRESS, deletable=True) == response
 
 
-def test_jobmanager_api_get_runner_health_not_found(jobmanager_api):
+def test_jobmanager_api_get_runner_health_not_found(
+    monkeypatch: pytest.MonkeyPatch, jobmanager_api: JobManagerAPI
+):
     """
     arrange: Create a jobmanager api object and stub the api client method to return a 404.
     act: Call get_runner_health method.
     assert: The method returns a JobManagerNotFound exception
     """
-    jobmanager_api._runners_api.get_runner_health_v1_runners_runner_id_health_get = MagicMock(
-        side_effect=NotFoundException("Runner not found")
+    monkeypatch.setattr(JobManagerAPI, "_create_api_client", MagicMock())
+    monkeypatch.setattr(
+        "jobmanager_client.RunnersApi.get_runner_health_v1_runners_runner_id_health_get",
+        MagicMock(side_effect=NotFoundException("Runner not found")),
     )
 
     with pytest.raises(JobManagerAPINotFoundError):
@@ -114,29 +119,38 @@ def test_jobmanager_api_get_runner_health_not_found(jobmanager_api):
         pytest.param(ValueError(), id="ValueError"),
     ],
 )
-def test_jobmanager_api_get_runner_health_error(jobmanager_api, exception):
+def test_jobmanager_api_get_runner_health_error(
+    monkeypatch: pytest.MonkeyPatch, jobmanager_api: JobManagerAPI, exception: Exception
+):
     """
     arrange: Create a jobmanager api object and stub the api client method to raise an exception.
     act: Call get_runner_health method.
     assert: The method raises the exception
     """
-    jobmanager_api._runners_api.get_runner_health_v1_runners_runner_id_health_get = MagicMock(
-        side_effect=exception
+    monkeypatch.setattr(JobManagerAPI, "_create_api_client", MagicMock())
+    monkeypatch.setattr(
+        "jobmanager_client.RunnersApi.get_runner_health_v1_runners_runner_id_health_get",
+        MagicMock(side_effect=exception),
     )
 
     with pytest.raises(JobManagerAPIError):
         jobmanager_api.get_runner_health(runner_id=123)
 
 
-def test_jobmanager_api_register_runner(jobmanager_api):
+def test_jobmanager_api_register_runner(
+    monkeypatch: pytest.MonkeyPatch, jobmanager_api: JobManagerAPI
+):
     """
     arrange: Create a jobmanager api object and stub the api client method.
     act: Call register_runner method.
     assert: The method returns a RunnerRegistration object.
     """
-    jobmanager_api._runners_api.register_runner_v1_runners_register_post = MagicMock(
-        return_value=RunnerRegisterResponse(id=123, token="token")
+    monkeypatch.setattr(JobManagerAPI, "_create_api_client", MagicMock())
+    monkeypatch.setattr(
+        "jobmanager_client.RunnersApi.register_runner_v1_runners_register_post",
+        MagicMock(return_value=RunnerRegisterResponse(id=123, token="token")),
     )
+
     response = jobmanager_api.register_runner(name="test-runner", labels=["label1", "label2"])
     assert response == RunnerRegistration(id=123, token="token")
 
@@ -149,14 +163,18 @@ def test_jobmanager_api_register_runner(jobmanager_api):
         pytest.param(ValueError(), id="ValueError"),
     ],
 )
-def test_jobmanager_api_register_runner_error(jobmanager_api, exception):
+def test_jobmanager_api_register_runner_error(
+    monkeypatch: pytest.MonkeyPatch, jobmanager_api: JobManagerAPI, exception: Exception
+):
     """
     arrange: Create a jobmanager api object and stub the api client method to raise an exception.
     act: Call get_runner_health method.
     assert: The method raises the exception
     """
-    jobmanager_api._runners_api.register_runner_v1_runners_register_post = MagicMock(
-        side_effect=exception
+    monkeypatch.setattr(JobManagerAPI, "_create_api_client", MagicMock())
+    monkeypatch.setattr(
+        "jobmanager_client.RunnersApi.register_runner_v1_runners_register_post",
+        MagicMock(side_effect=exception),
     )
 
     with pytest.raises(JobManagerAPIError):
@@ -171,33 +189,44 @@ def test_jobmanager_api_register_runner_error(jobmanager_api, exception):
         pytest.param(None, id="None"),
     ],
 )
-def test_jobmanager_api_get_job(jobmanager_api, status):
+def test_jobmanager_api_get_job(
+    monkeypatch: pytest.MonkeyPatch, jobmanager_api: JobManagerAPI, status: str
+):
     """
     arrange: Create a jobmanager api object and stub the api client method.
     act: Call get_runner_health method.
     assert: The method returns a Job object.
     """
-    jobmanager_api._jobs_api.get_job_v1_jobs_job_id_get = MagicMock(
-        return_value=JobRead(
-            status=status,
-            architecture="arm64",
-            base_series="jammy",
-            id=1,
-            requested_by="foobar",
-        )
+    monkeypatch.setattr(JobManagerAPI, "_create_api_client", MagicMock())
+    monkeypatch.setattr(
+        "jobmanager_client.JobsApi.get_job_v1_jobs_job_id_get",
+        MagicMock(
+            return_value=JobRead(
+                status=status,
+                architecture="arm64",
+                base_series="jammy",
+                id=1,
+                requested_by="foobar",
+            )
+        ),
     )
+
     response = jobmanager_api.get_job(job_id=123)
     assert Job(status=status) == response
 
 
-def test_jobmanager_api_get_job_not_found(jobmanager_api):
+def test_jobmanager_api_get_job_not_found(
+    monkeypatch: pytest.MonkeyPatch, jobmanager_api: JobManagerAPI
+):
     """
     arrange: Create a jobmanager api object and stub the api client method to return a 404.
     act: Call get_job method.
     assert: The method returns a JobManagerNotFound exception
     """
-    jobmanager_api._jobs_api.get_job_v1_jobs_job_id_get = MagicMock(
-        side_effect=NotFoundException("Job not found")
+    monkeypatch.setattr(JobManagerAPI, "_create_api_client", MagicMock())
+    monkeypatch.setattr(
+        "jobmanager_client.JobsApi.get_job_v1_jobs_job_id_get",
+        MagicMock(side_effect=NotFoundException("Job not found")),
     )
 
     with pytest.raises(JobManagerAPINotFoundError):
@@ -212,13 +241,18 @@ def test_jobmanager_api_get_job_not_found(jobmanager_api):
         pytest.param(ValueError(), id="ValueError"),
     ],
 )
-def test_jobmanager_api_get_job_error(jobmanager_api, exception):
+def test_jobmanager_api_get_job_error(
+    monkeypatch: pytest.MonkeyPatch, jobmanager_api: JobManagerAPI, exception: Exception
+):
     """
     arrange: Create a jobmanager api object and stub the api client method to raise an exception.
     act: Call get_job method.
     assert: The method raises the exception
     """
-    jobmanager_api._jobs_api.get_job_v1_jobs_job_id_get = MagicMock(side_effect=exception)
+    monkeypatch.setattr(JobManagerAPI, "_create_api_client", MagicMock())
+    monkeypatch.setattr(
+        "jobmanager_client.JobsApi.get_job_v1_jobs_job_id_get", MagicMock(side_effect=exception)
+    )
 
     with pytest.raises(JobManagerAPIError):
         jobmanager_api.get_job(job_id=123)
