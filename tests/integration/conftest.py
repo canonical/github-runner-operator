1aa16adabe510d152af89e4f47403442dbe3490b
Weii Wang <weii.wang@canonical.com>
2025-07-07 23:21:20 +0200

    Forward arbitrary TCP traffic using aproxy (#580)
    * Forward arbitrary TCP traffic using aproxy
    
    * Update integration tests and fix linting tests
    
    * Fix linting issue and update integration issues
    
    * Fix linting issues
    
    * Update e2e test
    
    * Add validator to aproxy_redirect_ports and aproxy_exclude_addresses
    
    * Use partition instead of split
    
    * Simplify _generate_cloud_init
    
    * Fix _generate_cloud_init
    
    * Simplify _generate_cloud_init
    
    * Fix e2e test
    
    * Apply suggestions from review comments
    
    * Small refactor over the charm_state.py
    
    * Fix unit tests
    
    * remove test for TCP proxy
    
    * Update e2e tests
    
    * Always exclude 127.0.0.0/8
    
    * Increase timeout for runner_manager_with_one_runner_fixture

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -29,6 +29,7 @@ from openstack.connection import Connection
 from pytest_operator.plugin import OpsTest
 
 from charm_state import (
+    APROXY_REDIRECT_PORTS_CONFIG_NAME,
     BASE_VIRTUAL_MACHINES_CONFIG_NAME,
     LABELS_CONFIG_NAME,
     OPENSTACK_CLOUDS_YAML_CONFIG_NAME,
@@ -461,6 +462,7 @@ async def app_openstack_runner_fixture(
                 OPENSTACK_NETWORK_CONFIG_NAME: network_name,
                 OPENSTACK_FLAVOR_CONFIG_NAME: flavor_name,
                 USE_APROXY_CONFIG_NAME: bool(openstack_http_proxy),
+                APROXY_REDIRECT_PORTS_CONFIG_NAME: "1-3127,3129-65535",
                 LABELS_CONFIG_NAME: app_name,
             },
             wait_idle=False,
