9f162d6737f490505cbb9e11ae7327511c5aa9c4
yhaliaw <43424755+yhaliaw@users.noreply.github.com>
2025-07-22 09:53:31 +0800

    Remove old pre-merge code

--- github-runner-manager/tests/unit/test_runner_scaler.py
+++ github-runner-manager/tests/unit/test_runner_scaler.py
@@ -46,10 +46,6 @@ from github_runner_manager.openstack_cloud.openstack_runner_manager import (
     OpenStackRunnerManagerConfig,
 )
 from github_runner_manager.platform.github_provider import PlatformRunnerState
-from github_runner_manager.reactive.process_manager import (
-    REACTIVE_RUNNER_CMD_LINE_PREFIX,
-    ReactiveRunnerError,
-)
 from github_runner_manager.reactive.types_ import ReactiveProcessConfig
 from tests.unit.mock_runner_managers import (
     MockCloudRunnerManager,
@@ -726,42 +722,3 @@ def test_delete_some_runners_in_reconcile(runner_manager: RunnerManager, user_in
     assert initial_mock_runners[2].instance_id in runner_dict
     # The runner without health information should not be deleted
     assert initial_mock_runners[4].instance_id in runner_dict
-
-
-def test_reactive_flush_success(
-    runner_scaler_reactive: RunnerScaler, mock_process_manager_subprocess_run: MagicMock
-):
-    """
-    arrange: Mock the subprocess run to succeed.
-    act: Run flush for reactive.
-    assert: Subprocess run executed a pkill for reactive processes.
-    """
-    mock_run = mock_process_manager_subprocess_run
-    mock_result = MagicMock()
-    mock_result.returncode = 0
-    mock_run.return_value = mock_result
-
-    runner_scaler_reactive.flush()
-
-    mock_run.assert_called_once_with(cmd=["pkill", "-f", REACTIVE_RUNNER_CMD_LINE_PREFIX])
-
-
-def test_reactive_flush_failed(
-    runner_scaler_reactive: RunnerScaler, mock_process_manager_subprocess_run: MagicMock
-):
-    """
-    arrange: Mock the subprocess run to fail.
-    act: Run flush for reactive.
-    assert: A error raised for the failed subprocess run.
-    """
-    mock_run = mock_process_manager_subprocess_run
-    mock_result = MagicMock()
-    mock_result.returncode = 10
-    mock_run.return_value = mock_result
-
-    with pytest.raises(ReactiveRunnerError) as err:
-        runner_scaler_reactive.flush()
-
-    assert "Failed to kill the reactive processes" in str(err.value)
-
-    mock_run.assert_called_once_with(cmd=["pkill", "-f", REACTIVE_RUNNER_CMD_LINE_PREFIX])
