afe723bc8890defd320924a47c44515265173459
charlie4284 <yangsoo.yoon@canonical.com>
2025-09-04 09:26:14 +0000

    test: microk8s wait ready timeout

--- tests/integration/setup-integration-tests.sh
+++ tests/integration/setup-integration-tests.sh
@@ -22,7 +22,7 @@ fi
 # Get preferred source IP address for metallb
 IPADDR=$( { ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc'; } )
 sudo microk8s enable "metallb:$IPADDR-$IPADDR" "hostpath-storage"
-microk8s status --wait-ready
+microk8s status --wait-ready --timeout=1200
 
 unset JUJU_CONTROLLER
 unset JUJU_MODEL
