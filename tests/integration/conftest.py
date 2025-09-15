ddf88c8db3d814ef2036089478978a0a3cc92813
yhaliaw <andrew.liaw@canonical.com>
2025-09-15 15:38:44 +0800

    Test fix with rebuild

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -421,7 +421,7 @@ async def image_builder_fixture(
             application_name=application_name,
             channel="latest/edge",
             # TODO: Temp switch to a fix revision
-            revision=119,
+            revision=120,
             config=image_builder_config,
         )
     else:
