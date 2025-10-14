7b01a2e5cedebfa0de66b9072a2da3aca323f151
Christopher Bartz <christopher.bartz@canonical.com>
2025-10-14 07:46:10 +0200

    disable kernel checks in needrestart

--- enable_fips.sh
+++ enable_fips.sh
@@ -11,3 +11,5 @@ truncate -s 0 /etc/machine-id
 rm /var/lib/dbus/machine-id
 rm -rf /var/lib/ubuntu-advantage/private
 rm -f /var/log/ubuntu-advantage*
+# disable kernel checks for need restart to avoid printing "Pending Kernel Upgrade"
+sudo sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = 0;/g" /etc/needrestart/needrestart.conf
