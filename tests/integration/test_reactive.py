fd53bf2355de818da353d20ad7c9226e3982f4b1
charlie4284 <yangsoo.yoon@canonical.com>
2026-01-05 13:02:23 +0800

    chore: update license header

--- tests/integration/test_reactive.py
+++ tests/integration/test_reactive.py
@@ -1,4 +1,4 @@
-#  Copyright 2025 Canonical Ltd.
+#  Copyright 2026 Canonical Ltd.
 #  See LICENSE file for licensing details.
 
 """Testing reactive mode. This is only supported for the OpenStack cloud."""
