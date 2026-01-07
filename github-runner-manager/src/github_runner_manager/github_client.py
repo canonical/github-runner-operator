92733c0eb92c096452d07538b094a7db19416261
charlie4284 <yangsoo.yoon@canonical.com>
2026-01-07 11:36:53 +0800

    add job debug info

--- github-runner-manager/src/github_runner_manager/github_client.py
+++ github-runner-manager/src/github_runner_manager/github_client.py
@@ -401,7 +401,11 @@ class GithubClient:
         status = job["status"]
         job_id = job["id"]
         logger.info(
-            "Fetched job info: id=%s, status=%s, conclusion=%s", job_id, status, conclusion
+            "Fetched job info: id=%s, status=%s, conclusion=%s, job: %s",
+            job_id,
+            status,
+            conclusion,
+            job,
         )
         return JobInfo(
             job_id=job_id,
