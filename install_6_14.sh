43ed5fc128b47f616458f1da3665c1e46ba309e1
Christopher Bartz <christopher.bartz@canonical.com>
2026-02-12 14:27:33 +0100

    hold packages and avoid Pending Kernel Upgrade warnings

--- install_6_14.sh
+++ install_6_14.sh
@@ -14,3 +14,9 @@ sudo apt-get install -y linux-generic-6.14
 KERNEL_ENTRY=$(sudo grep -oP "menuentry '\K[^']*6\.14[^']*" /boot/grub/grub.cfg | head -1)
 sudo grub-set-default "Advanced options for Ubuntu>${KERNEL_ENTRY}"
 sudo update-grub
+
+# Prevent apt upgrade from pulling in a newer HWE kernel
+sudo apt-mark hold linux-generic-hwe-24.04 linux-image-generic-hwe-24.04
+
+# Suppress misleading "Pending Kernel Upgrade" warnings from needrestart
+sudo sed -i "s/#\$nrconf{kernelhints} = -1;/\$nrconf{kernelhints} = 0;/g" /etc/needrestart/needrestart.conf
