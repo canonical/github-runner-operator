1b1c9127e7402c91ab9f7264b9caacc569c2c907
Christopher Bartz <christopher.bartz@canonical.com>
2026-02-12 16:31:53 +0100

    add saved line

--- install_6_14.sh
+++ install_6_14.sh
@@ -12,6 +12,7 @@ sudo apt-get install -y linux-generic-6.14
 
 # Pin GRUB to boot the 6.14 kernel instead of the newer HWE kernel
 KERNEL_ENTRY=$(sudo grep -oP "menuentry '\K[^']*6\.14[^']*" /boot/grub/grub.cfg | head -1)
+sudo sed -i 's/^GRUB_DEFAULT=.*/GRUB_DEFAULT=saved/' /etc/default/grub
 sudo grub-set-default "Advanced options for Ubuntu>${KERNEL_ENTRY}"
 sudo update-grub
 
