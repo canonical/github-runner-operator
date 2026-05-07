f3a4349e49be4abcd7b98568a25443b6b7cbfa82
yhaliaw <andrew.liaw@canonical.com>
2026-05-07 15:29:30 +0800

    Add rust resolute runner image building

---
+++ rust_resolute.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+#  Copyright 2026 Canonical Ltd.
+#  See LICENSE file for licensing details.
+#
+
+sudo apt-get update
+sudo apt-get install -yq rustup docker.io docker-buildx
