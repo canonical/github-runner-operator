a0f0a055e4efc885535e497ba8781ad75c867264
yhaliaw <andrew.liaw@canonical.com>
2026-05-22 15:52:13 +0800

    Add -y for add PAA for dotnet

--- rust_resolute.sh
+++ rust_resolute.sh
@@ -8,6 +8,6 @@ set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo add-apt-repository ppa:dotnet/backports
+sudo add-apt-repository -y ppa:dotnet/backports
 sudo apt update
 sudo apt-get install -yq rustup docker.io docker-buildx
